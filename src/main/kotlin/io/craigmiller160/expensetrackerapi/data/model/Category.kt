4729e35d3887aa7c882e6e4de05e3fc5c77b5f00
Craig Miller <craigmiller160@gmail.com>
2022-06-17 16:41:53 -0400

    Updated category

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/Category.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/Category.kt
@@ -1,5 +1,10 @@
 package io.craigmiller160.expensetrackerapi.data.model
 
 import io.craigmiller160.expensetrackerapi.common.data.typeid.ids.CategoryId
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
 
-data class Category(val id: CategoryId = CategoryId())
+@Entity
+@Table(name = "categories")
+data class Category(val name: String, @Id val id: CategoryId = CategoryId())
