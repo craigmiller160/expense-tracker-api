8d30c0afb4dc74f0d023b12ada3a02be781a1e9e
Craig Miller <craigmiller160@gmail.com>
2022-06-17 16:03:13 -0400

    Added tls config

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/ExpenseTrackerApiApplication.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/ExpenseTrackerApiApplication.kt
@@ -1,10 +1,16 @@
 package io.craigmiller160.expensetrackerapi
 
+import io.craigmiller160.webutils.tls.TlsConfigurer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication class ExpenseTrackerApiApplication
 
+private const val TRUST_STORE_TYPE = "JKS"
+private const val TRUST_STORE_PATH = "truststore.jks"
+private const val TRUST_STORE_PASSWORD = "changeit"
+
 fun main(args: Array<String>) {
+  TlsConfigurer.configureTlsTrustStore(TRUST_STORE_PATH, TRUST_STORE_TYPE, TRUST_STORE_PASSWORD)
   runApplication<ExpenseTrackerApiApplication>(*args)
 }
