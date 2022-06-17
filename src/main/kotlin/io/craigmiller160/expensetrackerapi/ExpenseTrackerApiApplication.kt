1efae1dfa6085a146c7ca75c78e5398493afeeb7
Craig Miller <craigmiller160@gmail.com>
2022-06-17 15:48:57 -0400

    Cleaned up file formatting

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/ExpenseTrackerApiApplication.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/ExpenseTrackerApiApplication.kt
@@ -3,9 +3,8 @@ package io.craigmiller160.expensetrackerapi
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-class ExpenseTrackerApiApplication
+@SpringBootApplication class ExpenseTrackerApiApplication
 
 fun main(args: Array<String>) {
-    runApplication<ExpenseTrackerApiApplication>(*args)
+  runApplication<ExpenseTrackerApiApplication>(*args)
 }
