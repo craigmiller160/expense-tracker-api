738dcfad7ba671fa6fa6efaeb138f9a0d73e983a
Craig Miller <craigmiller160@gmail.com>
2022-06-18 17:07:22 -0400

    Progress

--- src/test/kotlin/io/craigmiller160/expensetrackerapi/BaseIntegrationTest.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/BaseIntegrationTest.kt
@@ -1,8 +1,15 @@
 package io.craigmiller160.expensetrackerapi
 
+import io.craigmiller160.oauth2.config.OAuth2Config
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@SpringBootTest @AutoConfigureMockMvc @ExtendWith(SpringExtension::class) class BaseIntegrationTest
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension::class)
+class BaseIntegrationTest {
+  @MockBean private lateinit var oAuth2Config: OAuth2Config
+}
