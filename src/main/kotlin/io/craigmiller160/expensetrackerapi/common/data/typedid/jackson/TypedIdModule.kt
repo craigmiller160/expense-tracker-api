019535e692bb334ea8f916b368353787d8777afa
Craig Miller <craigmiller160@gmail.com>
2022-06-18 15:36:57 -0400

    Made module into component

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/common/data/typedid/jackson/TypedIdModule.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/common/data/typedid/jackson/TypedIdModule.kt
@@ -2,7 +2,9 @@ package io.craigmiller160.expensetrackerapi.common.data.typedid.jackson
 
 import com.fasterxml.jackson.databind.module.SimpleModule
 import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
+import org.springframework.stereotype.Component
 
+@Component
 class TypedIdModule : SimpleModule("TypedIdModule") {
   init {
     addSerializer(TypedIdSerializer())
