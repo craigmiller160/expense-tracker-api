e46ecdc040f45291ff3b97f809ecca2dbfc959c4
Craig Miller <craigmiller160@gmail.com>
2022-06-18 17:01:42 -0400

    Implemented get import types method

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/service/TransactionImportService.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/service/TransactionImportService.kt
@@ -1,8 +1,10 @@
 package io.craigmiller160.expensetrackerapi.service
 
+import io.craigmiller160.expensetrackerapi.web.types.ImportTypeResponse
 import org.springframework.stereotype.Service
 
 @Service
 class TransactionImportService {
-  fun getImportTypes() {}
+  fun getImportTypes(): List<ImportTypeResponse> =
+      TransactionImportType.values().map { ImportTypeResponse(it.name, it.displayName) }
 }
--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/controller/TransactionImportController.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/controller/TransactionImportController.kt
@@ -1,6 +1,9 @@
 package io.craigmiller160.expensetrackerapi.web.controller
 
+import io.craigmiller160.expensetrackerapi.service.TransactionImportService
 import io.craigmiller160.expensetrackerapi.service.TransactionImportType
+import io.craigmiller160.expensetrackerapi.web.types.ImportTypeResponse
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -9,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile
 
 @RestController
 @RequestMapping("/transaction-import")
-class TransactionImportController {
+class TransactionImportController(private val transactionImportService: TransactionImportService) {
   @PostMapping
   fun importTransactions(
       @RequestParam("type") type: TransactionImportType,
@@ -17,4 +20,7 @@ class TransactionImportController {
   ) {
     TODO()
   }
+
+  @GetMapping("/types")
+  fun getImportTypes(): List<ImportTypeResponse> = transactionImportService.getImportTypes()
 }
