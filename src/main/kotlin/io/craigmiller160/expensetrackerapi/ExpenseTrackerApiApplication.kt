c2e9dfb4a51f363d8dcfdce65ccd36829dc3df33
Craig Miller <craigmiller160@gmail.com>
2022-06-18 15:13:12 -0400

    Added temp logic to validate stuff

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/ExpenseTrackerApiApplication.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/ExpenseTrackerApiApplication.kt
@@ -1,10 +1,21 @@
 package io.craigmiller160.expensetrackerapi
 
+import io.craigmiller160.expensetrackerapi.data.model.Category
+import io.craigmiller160.expensetrackerapi.data.repository.CategoryRepository
 import io.craigmiller160.webutils.tls.TlsConfigurer
+import javax.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication class ExpenseTrackerApiApplication
+@SpringBootApplication
+class ExpenseTrackerApiApplication(private val categoryRepository: CategoryRepository) {
+  // TODO delete this
+  @PostConstruct
+  fun temp() {
+    val category = Category("Cat1")
+    categoryRepository.save(category)
+  }
+}
 
 private const val TRUST_STORE_TYPE = "JKS"
 private const val TRUST_STORE_PATH = "truststore.jks"
