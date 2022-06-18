1c8b94faf03e0f3c14b0cc4d58c0bd9ac6d8082e
Craig Miller <craigmiller160@gmail.com>
2022-06-18 14:45:59 -0400

    Working on type descriptor

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/common/data/typeid/TypedId.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/common/data/typeid/TypedId.kt
@@ -4,5 +4,8 @@ import java.io.Serializable
 import java.util.UUID
 
 abstract class TypedId(val uuid: UUID) : Serializable {
+  // TODO don't forget the jackson serializer
+
+  constructor(id: String) : this(UUID.fromString(id))
   override fun toString(): String = uuid.toString()
 }
---
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/common/data/typeid/jpatype/TypedIdDescriptor.kt
@@ -0,0 +1,23 @@
+package io.craigmiller160.expensetrackerapi.common.data.typeid.jpatype
+
+import io.craigmiller160.expensetrackerapi.common.data.typeid.TypedId
+import org.hibernate.type.descriptor.WrapperOptions
+import org.hibernate.type.descriptor.java.AbstractTypeDescriptor
+import org.hibernate.type.descriptor.java.UUIDTypeDescriptor
+
+class TypedIdDescriptor : AbstractTypeDescriptor<TypedId>(TypedId::class.java) {
+  companion object {
+    val INSTANCE = TypedIdDescriptor()
+  }
+  override fun fromString(string: String): TypedId {
+    TODO("Not yet implemented")
+  }
+
+  override fun <X : Any> wrap(value: X, options: WrapperOptions): TypedId {
+    TODO("Not yet implemented")
+  }
+
+  override fun <X : Any> unwrap(value: TypedId, type: Class<X>, options: WrapperOptions): X {
+    return UUIDTypeDescriptor.INSTANCE.unwrap(value.uuid, type, options)
+  }
+}
