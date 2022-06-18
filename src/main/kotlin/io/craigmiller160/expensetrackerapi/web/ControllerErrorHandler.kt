c2a1e7e8d5273b52e9f14c14a8f5cf5bdb507122
Craig Miller <craigmiller160@gmail.com>
2022-06-18 16:13:15 -0400

    Cleanup

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/ControllerErrorHandler.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/ControllerErrorHandler.kt
@@ -5,6 +5,7 @@ import java.time.ZoneId
 import java.time.ZonedDateTime
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.AccessDeniedException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.context.request.RequestContextHolder
@@ -13,10 +14,17 @@ import org.springframework.web.context.request.ServletRequestAttributes
 @ControllerAdvice
 class ControllerErrorHandler {
   private val log = LoggerFactory.getLogger(ControllerErrorHandler::class.java)
+
   @ExceptionHandler(Exception::class)
   fun exception(ex: Exception): ResponseEntity<ErrorResponse> {
     log.error(ex.message, ex)
     return createErrorResponse(500, ex.message ?: "")
+  }
+
+  @ExceptionHandler(AccessDeniedException::class)
+  fun accessDeniedException(ex: AccessDeniedException): ResponseEntity<ErrorResponse> {
+    log.error("Access Denied", ex)
+    return createErrorResponse(403, "Access Denied")
   }
 
   private fun createErrorResponse(status: Int, message: String): ResponseEntity<ErrorResponse> {
