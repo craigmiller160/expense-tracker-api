6688d7d02428755fe156c25dcf0f01fe031e0f9e
Craig Miller <craigmiller160@gmail.com>
2022-06-19 17:12:19 -0400

    Some progress with interface design

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/core/MutableEntity.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/core/MutableEntity.kt
@@ -3,23 +3,16 @@ package io.craigmiller160.expensetrackerapi.data.model.core
 import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.jpatype.TypedIdJpaType
 import java.time.ZonedDateTime
-import javax.persistence.Id
-import javax.persistence.MappedSuperclass
 import javax.persistence.PreUpdate
-import javax.persistence.Version
 import org.hibernate.annotations.TypeDef
 
-@MappedSuperclass
 @TypeDef(defaultForType = TypedId::class, typeClass = TypedIdJpaType::class)
-abstract class AbstractMutableEntity<T>(
-    @Id
-    //    @Type(type =
-    // "io.craigmiller160.expensetrackerapi.common.data.typedid.jpatype.TypedIdJpaType")
-    val id: TypedId<T> = TypedId(),
-    val created: ZonedDateTime = ZonedDateTime.now(),
-    var updated: ZonedDateTime = ZonedDateTime.now(),
-    @Version val version: Long = 1
-) {
+interface MutableEntity<T> {
+  val id: TypedId<T>
+  val created: ZonedDateTime
+  var updated: ZonedDateTime
+  val version: Long
+
   @PreUpdate
   fun onPreUpdate() {
     updated = ZonedDateTime.now()
--- src/test/kotlin/io/craigmiller160/expensetrackerapi/data/model/Resident.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/data/model/Resident.kt
@@ -1,9 +1,19 @@
 package io.craigmiller160.expensetrackerapi.data.model
 
-import io.craigmiller160.expensetrackerapi.data.model.core.AbstractMutableEntity
+import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
+import io.craigmiller160.expensetrackerapi.data.model.core.MutableEntity
+import java.time.ZonedDateTime
 import javax.persistence.Entity
+import javax.persistence.Id
 import javax.persistence.Table
+import javax.persistence.Version
 
 @Entity
 @Table(name = "residents")
-data class Resident(val name: String) : AbstractMutableEntity<ResidentId>()
+data class Resident(
+    val name: String,
+    @Id override val id: TypedId<ResidentId> = TypedId(),
+    override val created: ZonedDateTime = ZonedDateTime.now(),
+    override var updated: ZonedDateTime = ZonedDateTime.now(),
+    @Version override val version: Long = 1
+) : MutableEntity<ResidentId>
