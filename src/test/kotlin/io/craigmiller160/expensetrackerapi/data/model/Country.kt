1e3e7c78c6ed00312fb34e5c679465364c9e0b66
Craig Miller <craigmiller160@gmail.com>
2022-06-19 17:17:48 -0400

    Updated test entity

--- src/test/kotlin/io/craigmiller160/expensetrackerapi/data/model/Country.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/data/model/Country.kt
@@ -1,9 +1,16 @@
 package io.craigmiller160.expensetrackerapi.data.model
 
-import io.craigmiller160.expensetrackerapi.data.model.core.AbstractImmutableEntity
+import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
+import io.craigmiller160.expensetrackerapi.data.model.core.ImmutableEntity
+import java.time.ZonedDateTime
 import javax.persistence.Entity
+import javax.persistence.Id
 import javax.persistence.Table
 
 @Entity
 @Table(name = "countries")
-data class Country(val name: String) : AbstractImmutableEntity<CountryId>()
+data class Country(
+    val name: String,
+    @Id override val id: TypedId<CountryId> = TypedId(),
+    override val created: ZonedDateTime = ZonedDateTime.now()
+) : ImmutableEntity<CountryId>
