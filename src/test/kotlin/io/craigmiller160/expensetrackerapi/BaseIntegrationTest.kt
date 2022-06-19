75d04dd2eaf6d5adb94fb88e768fcfb7c19528f4
Craig Miller <craigmiller160@gmail.com>
2022-06-19 12:54:29 -0400

    More test setup improvement

--- src/test/kotlin/io/craigmiller160/expensetrackerapi/BaseIntegrationTest.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/BaseIntegrationTest.kt
@@ -1,7 +1,10 @@
 package io.craigmiller160.expensetrackerapi
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.nimbusds.jose.jwk.JWKSet
+import io.craigmiller160.expensetrackerapi.testutils.KeyUtils
 import io.craigmiller160.oauth2.config.OAuth2Config
+import java.security.KeyPair
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -14,6 +17,11 @@ import org.springframework.test.web.servlet.MockMvc
 @AutoConfigureMockMvc
 @ExtendWith(SpringExtension::class)
 class BaseIntegrationTest {
+  companion object {
+    protected val keyPair: KeyPair = KeyUtils.createKeyPair()
+    protected val jwkSet: JWKSet = KeyUtils.createJwkSet(keyPair)
+  }
+
   @MockBean private lateinit var oAuth2Config: OAuth2Config
   @Autowired protected lateinit var mockMvc: MockMvc
   @Autowired protected lateinit var objectMapper: ObjectMapper
