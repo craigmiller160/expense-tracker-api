c6d436fe38cf004f1f123b192ed9534874be89b7
Craig Miller <craigmiller160@gmail.com>
2022-06-19 17:21:11 -0400

    Cleaned up other entities

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/Category.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/Category.kt
@@ -1,10 +1,21 @@
 package io.craigmiller160.expensetrackerapi.data.model
 
+import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.CategoryId
-import io.craigmiller160.expensetrackerapi.data.model.core.AbstractMutableEntity
+import io.craigmiller160.expensetrackerapi.data.model.core.MutableEntity
+import java.time.ZonedDateTime
 import javax.persistence.Entity
+import javax.persistence.Id
 import javax.persistence.Table
+import javax.persistence.Version
 
 @Entity
 @Table(name = "categories")
-data class Category(val name: String, val userId: Long) : AbstractMutableEntity<CategoryId>()
+data class Category(
+    val name: String,
+    val userId: Long,
+    @Id override val id: TypedId<CategoryId> = TypedId(),
+    override val created: ZonedDateTime = ZonedDateTime.now(),
+    override var updated: ZonedDateTime = ZonedDateTime.now(),
+    @Version override val version: Long = 1
+) : MutableEntity<CategoryId>
--- src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/Transaction.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/Transaction.kt
@@ -3,11 +3,14 @@ package io.craigmiller160.expensetrackerapi.data.model
 import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.CategoryId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.TransactionId
-import io.craigmiller160.expensetrackerapi.data.model.core.AbstractMutableEntity
+import io.craigmiller160.expensetrackerapi.data.model.core.MutableEntity
 import java.math.BigDecimal
 import java.time.LocalDate
+import java.time.ZonedDateTime
 import javax.persistence.Entity
+import javax.persistence.Id
 import javax.persistence.Table
+import javax.persistence.Version
 
 @Entity
 @Table(name = "transactions")
@@ -17,5 +20,9 @@ data class Transaction(
     val description: String,
     val amount: BigDecimal,
     val confirmed: Boolean,
-    val categoryId: TypedId<CategoryId>? = null
-) : AbstractMutableEntity<TransactionId>()
+    val categoryId: TypedId<CategoryId>? = null,
+    @Id override val id: TypedId<TransactionId> = TypedId(),
+    override val created: ZonedDateTime = ZonedDateTime.now(),
+    override var updated: ZonedDateTime = ZonedDateTime.now(),
+    @Version override val version: Long = 1
+) : MutableEntity<TransactionId>
