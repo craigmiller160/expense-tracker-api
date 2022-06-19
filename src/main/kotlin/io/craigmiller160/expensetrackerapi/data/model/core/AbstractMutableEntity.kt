1a2d9569602311e24293570fde7d75f87682e497
Craig Miller <craigmiller160@gmail.com>
2022-06-19 15:46:40 -0400

    Cleanup

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/core/AbstractMutableEntity.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/core/AbstractMutableEntity.kt
@@ -6,19 +6,16 @@ import javax.persistence.Id
 import javax.persistence.MappedSuperclass
 import javax.persistence.PreUpdate
 import javax.persistence.Version
-import org.hibernate.annotations.OptimisticLocking
 import org.hibernate.annotations.Type
-import org.springframework.data.annotation.ReadOnlyProperty
 
 @MappedSuperclass
-@OptimisticLocking
 abstract class AbstractMutableEntity<T>(
     @Id
     @Type(type = "io.craigmiller160.expensetrackerapi.common.data.typedid.jpatype.TypedIdJpaType")
     val id: TypedId<T> = TypedId(),
     val created: ZonedDateTime = ZonedDateTime.now(),
     var updated: ZonedDateTime = ZonedDateTime.now(),
-    @Version @ReadOnlyProperty val version: Long = 1
+    @Version val version: Long = 1
 ) {
   @PreUpdate
   fun onPreUpdate() {
