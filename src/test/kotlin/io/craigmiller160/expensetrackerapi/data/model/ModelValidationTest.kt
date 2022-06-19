7874077490fc4b3bbee00752bb8f5ebeee76122f
Craig Miller <craigmiller160@gmail.com>
2022-06-19 15:20:15 -0400

    More test setup

--- src/test/kotlin/io/craigmiller160/expensetrackerapi/data/model/ModelValidationTest.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/data/model/ModelValidationTest.kt
@@ -2,9 +2,11 @@ package io.craigmiller160.expensetrackerapi.data.model
 
 import io.craigmiller160.expensetrackerapi.BaseIntegrationTest
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 
 /** Special test class for validating that the TypedId & parent model types work. */
 class ModelValidationTest : BaseIntegrationTest() {
+  @Autowired private lateinit var countryRepository: CountryRepository
   @Test
   fun `immutable entity inserts but cannot be updated`() {
     TODO("Finish this")
--- src/test/kotlin/io/craigmiller160/expensetrackerapi/data/model/ResidentRepository.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/data/model/ResidentRepository.kt
@@ -3,4 +3,4 @@ package io.craigmiller160.expensetrackerapi.data.model
 import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface CountryRepository : JpaRepository<Country, TypedId<CountryId>>
+interface ResidentRepository : JpaRepository<Resident, TypedId<ResidentId>>
