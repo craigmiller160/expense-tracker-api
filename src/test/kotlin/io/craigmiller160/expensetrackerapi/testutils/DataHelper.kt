fb0af8fd165569236e2dc8194f88bcc11c204a45
Craig Miller <craigmiller160@gmail.com>
2022-06-20 15:38:24 -0400

    Test data setup

--- src/test/kotlin/io/craigmiller160/expensetrackerapi/testutils/DataHelper.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/testutils/DataHelper.kt
@@ -1,7 +1,11 @@
 package io.craigmiller160.expensetrackerapi.testutils
 
 import com.github.javafaker.Faker
+import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
+import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.CategoryId
+import io.craigmiller160.expensetrackerapi.data.model.Category
 import io.craigmiller160.expensetrackerapi.data.model.Transaction
+import io.craigmiller160.expensetrackerapi.data.repository.CategoryRepository
 import io.craigmiller160.expensetrackerapi.data.repository.TransactionRepository
 import java.math.BigDecimal
 import java.time.LocalDate
@@ -10,17 +14,28 @@ import java.util.concurrent.TimeUnit
 import org.springframework.stereotype.Component
 
 @Component
-class DataHelper(private val transactionRepository: TransactionRepository) {
+class DataHelper(
+    private val transactionRepository: TransactionRepository,
+    private val categoryRepository: CategoryRepository
+) {
   private val faker = Faker()
-  fun createTransaction(userId: Long): Transaction {
-    val transaction =
-        Transaction(
-            userId = userId,
-            expenseDate =
-                LocalDate.ofInstant(
-                    faker.date().past(500, TimeUnit.DAYS).toInstant(), ZoneId.systemDefault()),
-            description = faker.company().name(),
-            amount = BigDecimal(faker.commerce().price()))
-    return transactionRepository.saveAndFlush(transaction)
-  }
+  fun createTransaction(userId: Long, categoryId: TypedId<CategoryId>? = null): Transaction =
+      transactionRepository.saveAndFlush(
+          Transaction(
+              userId = userId,
+              expenseDate =
+                  LocalDate.ofInstant(
+                      faker.date().past(500, TimeUnit.DAYS).toInstant(), ZoneId.systemDefault()),
+              description = faker.company().name(),
+              amount = BigDecimal(faker.commerce().price()),
+              categoryId = categoryId))
+
+  fun createCategory(userId: Long, name: String): Category =
+      categoryRepository.saveAndFlush(Category(userId = userId, name = name))
+
+  fun createDefaultCategories(userId: Long): List<Category> =
+      listOf(
+          createCategory(userId, "Shopping"),
+          createCategory(userId, "Restaurant"),
+          createCategory(userId, "Entertainment"))
 }
--- src/test/kotlin/io/craigmiller160/expensetrackerapi/web/controller/TransactionControllerTest.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/web/controller/TransactionControllerTest.kt
@@ -1,9 +1,31 @@
 package io.craigmiller160.expensetrackerapi.web.controller
 
 import io.craigmiller160.expensetrackerapi.BaseIntegrationTest
+import io.craigmiller160.expensetrackerapi.data.model.Category
+import io.craigmiller160.expensetrackerapi.data.model.Transaction
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class TransactionControllerTest : BaseIntegrationTest() {
+  private lateinit var user1Categories: List<Category>
+  private lateinit var user1Transactions: List<Transaction>
+
+  @BeforeEach
+  fun setup() {
+    user1Categories = dataHelper.createDefaultCategories(1L)
+
+    user1Transactions =
+        (0..12)
+            .map { index ->
+              if (index % 2 == 0) {
+                dataHelper.createTransaction(1L, user1Categories[index % 3].id)
+              } else {
+                dataHelper.createTransaction(2L)
+              }
+            }
+            .partition { it.userId == 1L }
+            .first
+  }
   @Test
   fun `search - confirmed transactions only`() {
     TODO()
