c78615850d3ae13155ebe41eeb2e2cc239f13317
Craig Miller <craigmiller160@gmail.com>
2022-06-20 12:00:53 -0400

    Got parsing to work without an error

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/service/parsing/AbstractCsvTransactionParser.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/service/parsing/AbstractCsvTransactionParser.kt
@@ -2,21 +2,22 @@ package io.craigmiller160.expensetrackerapi.service.parsing
 
 import arrow.core.Either
 import arrow.core.sequence
+import com.opencsv.CSVReader
 import io.craigmiller160.expensetrackerapi.common.error.InvalidImportException
 import io.craigmiller160.expensetrackerapi.data.model.Transaction
 import io.craigmiller160.expensetrackerapi.function.TryEither
+import java.io.StringReader
 
 typealias FieldExtractor = (index: Int, name: String) -> TryEither<String>
 
 abstract class AbstractCsvTransactionParser : TransactionParser {
 
+  // TODO no need to convert to string first
   override fun parse(userId: Long, transactions: String): TryEither<List<Transaction>> =
-      transactions
-          .trim()
-          .split("\n")
+      CSVReader(StringReader(transactions))
+          .readAll()
           .asSequence()
           .drop(1)
-          .map { line -> line.split(",").map { it.trim() } }
           .map { prepareFieldExtractor(it) }
           .mapIndexed { index, fieldExtractor ->
             getTransaction(userId, fieldExtractor).mapLeft {
@@ -25,7 +26,7 @@ abstract class AbstractCsvTransactionParser : TransactionParser {
           }
           .sequence()
 
-  private fun prepareFieldExtractor(fields: List<String>): FieldExtractor = { index, name ->
+  private fun prepareFieldExtractor(fields: Array<String>): FieldExtractor = { index, name ->
     Either.catch { fields[index] }
         .mapLeft { InvalidImportException("Missing field $name at CSV row index $index") }
   }
