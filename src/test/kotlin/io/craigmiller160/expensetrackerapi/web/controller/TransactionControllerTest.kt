bf300825126ecd37a98957349618fc753170de38
Craig Miller <craigmiller160@gmail.com>
2022-06-20 15:47:53 -0400

    More test setup

--- src/test/kotlin/io/craigmiller160/expensetrackerapi/web/controller/TransactionControllerTest.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/web/controller/TransactionControllerTest.kt
@@ -3,10 +3,14 @@ package io.craigmiller160.expensetrackerapi.web.controller
 import io.craigmiller160.expensetrackerapi.BaseIntegrationTest
 import io.craigmiller160.expensetrackerapi.data.model.Category
 import io.craigmiller160.expensetrackerapi.data.model.Transaction
+import io.craigmiller160.expensetrackerapi.data.repository.TransactionRepository
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 
 class TransactionControllerTest : BaseIntegrationTest() {
+  @Autowired private lateinit var transactionRepository: TransactionRepository
+
   private lateinit var user1Categories: List<Category>
   private lateinit var user1Transactions: List<Transaction>
 
@@ -18,13 +22,16 @@ class TransactionControllerTest : BaseIntegrationTest() {
         (0..12)
             .map { index ->
               if (index % 2 == 0) {
-                dataHelper.createTransaction(1L, user1Categories[index % 3].id)
+                dataHelper.createTransaction(1L)
               } else {
                 dataHelper.createTransaction(2L)
               }
             }
             .partition { it.userId == 1L }
-            .first
+            .first.mapIndexed { index, transaction ->
+              transactionRepository.saveAndFlush(
+                  transaction.copy(categoryId = user1Categories[index % 3].id))
+            }
   }
   @Test
   fun `search - confirmed transactions only`() {
