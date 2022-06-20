5df72f7522138aba5dc70e7efee8fdf4b2a499b1
Craig Miller <craigmiller160@gmail.com>
2022-06-20 12:39:40 -0400

    Added includeRecord implementation

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/service/parsing/ChaseCsvTransactionParser.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/service/parsing/ChaseCsvTransactionParser.kt
@@ -2,6 +2,7 @@ package io.craigmiller160.expensetrackerapi.service.parsing
 
 import io.craigmiller160.expensetrackerapi.data.model.Transaction
 import io.craigmiller160.expensetrackerapi.function.TryEither
+import io.craigmiller160.expensetrackerapi.function.flatMapCatch
 import org.springframework.stereotype.Component
 
 @Component
@@ -12,4 +13,8 @@ class ChaseCsvTransactionParser : AbstractCsvTransactionParser() {
   ): TryEither<Transaction> {
     TODO("Not yet implemented")
   }
+
+  /** Include only negative amounts to prevent adding deposits as expenses */
+  override fun includeRecord(fieldExtractor: FieldExtractor): TryEither<Boolean> =
+      fieldExtractor(3, "amount").flatMapCatch { amount -> amount.toDouble() < 0 }
 }
