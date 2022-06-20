cc772bc95a2eb9a364fee5fbc53051b3605fdbab
Craig Miller <craigmiller160@gmail.com>
2022-06-20 12:12:45 -0400

    More work on test

--- src/test/kotlin/io/craigmiller160/expensetrackerapi/BaseIntegrationTest.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/BaseIntegrationTest.kt
@@ -16,8 +16,10 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest
+@Transactional
 @AutoConfigureMockMvc
 @ExtendWith(SpringExtension::class)
 class BaseIntegrationTest {
--- src/test/kotlin/io/craigmiller160/expensetrackerapi/web/controller/TransactionImportControllerTest.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/web/controller/TransactionImportControllerTest.kt
@@ -3,16 +3,20 @@ package io.craigmiller160.expensetrackerapi.web.controller
 import arrow.core.Either
 import arrow.core.flatMap
 import io.craigmiller160.expensetrackerapi.BaseIntegrationTest
+import io.craigmiller160.expensetrackerapi.data.repository.TransactionRepository
 import io.craigmiller160.expensetrackerapi.service.TransactionImportType
 import io.craigmiller160.expensetrackerapi.testutils.ResourceUtils
 import io.craigmiller160.expensetrackerapi.web.types.ImportTypeResponse
 import io.kotest.assertions.arrow.core.shouldBeRight
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.multipart
 
 class TransactionImportControllerTest : BaseIntegrationTest() {
+  @Autowired private lateinit var transactionRepository: TransactionRepository
   @Test
   fun getImportTypes() {
     val expectedResponse =
@@ -40,9 +44,15 @@ class TransactionImportControllerTest : BaseIntegrationTest() {
                   header("Content-Type", MediaType.MULTIPART_FORM_DATA_VALUE)
                   file("file", bytes)
                 }
-                .andExpect { status { isOk() } }
+                .andExpect {
+                  status { isOk() }
+                  content { json("""{"transactionsImported":57}""") }
+                }
           }
         }
         .shouldBeRight()
+
+    val transactions = transactionRepository.findAllByOrderByExpenseDateAsc()
+    assertThat(transactions).hasSize(57)
   }
 }
