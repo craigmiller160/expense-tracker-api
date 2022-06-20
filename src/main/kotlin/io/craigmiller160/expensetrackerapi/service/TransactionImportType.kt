3d76fc530c30ad8a0e9fb35f0ec681a1f06bfc9b
Craig Miller <craigmiller160@gmail.com>
2022-06-20 12:19:48 -0400

    Added chase import type

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/service/TransactionImportType.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/service/TransactionImportType.kt
@@ -1,5 +1,6 @@
 package io.craigmiller160.expensetrackerapi.service
 
 enum class TransactionImportType(val displayName: String) {
-  DISCOVER_CSV("Discover (CSV)")
+  DISCOVER_CSV("Discover (CSV)"),
+  CHASE_CSV("Chase (CSV)")
 }
