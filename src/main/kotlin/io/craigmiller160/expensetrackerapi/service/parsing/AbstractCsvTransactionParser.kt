f77f218bb8d5981522ecc2b9206862cc306796db
Craig Miller <craigmiller160@gmail.com>
2022-06-20 12:37:12 -0400

    Revert "Refactored csv parser to support filtering"
    This reverts commit d4d898edc9883155394859e62bca887fa764f0b5.

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/service/parsing/AbstractCsvTransactionParser.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/service/parsing/AbstractCsvTransactionParser.kt
@@ -1,8 +1,6 @@
 package io.craigmiller160.expensetrackerapi.service.parsing
 
 import arrow.core.Either
-import arrow.core.flatMap
-import arrow.core.getOrElse
 import arrow.core.sequence
 import com.opencsv.CSVReader
 import io.craigmiller160.expensetrackerapi.common.error.InvalidImportException
@@ -21,41 +19,20 @@ abstract class AbstractCsvTransactionParser : TransactionParser {
           .asSequence()
           .drop(1)
           .map { prepareFieldExtractor(it) }
-          .map { includeRecord(it).map { include -> FilteredFieldExtractor(it, include) } }
-          .filter { handleEitherFilter(it) }
-          .map { either -> either.map { it.fieldExtractor } }
-          .mapIndexed { index, either -> mapToTransaction(index, userId, either) }
+          .mapIndexed { index, fieldExtractor ->
+            getTransaction(userId, fieldExtractor).mapLeft {
+              InvalidImportException("Error parsing CSV record, row ${index + 2}", it)
+            }
+          }
           .sequence()
-
-  private fun mapToTransaction(
-      index: Int,
-      userId: Long,
-      fieldExtractor: TryEither<FieldExtractor>
-  ): TryEither<Transaction> =
-      fieldExtractor.flatMap {
-        getTransaction(userId, it).mapLeft {
-          InvalidImportException("Error parsing CSV record, row ${index + 2}", it)
-        }
-      }
-
-  private fun handleEitherFilter(either: TryEither<FilteredFieldExtractor>): Boolean =
-      either.map { it.include }.getOrElse { true }
 
   private fun prepareFieldExtractor(fields: Array<String>): FieldExtractor = { index, name ->
     Either.catch { fields[index] }
         .mapLeft { InvalidImportException("Missing field $name at CSV row index $index") }
   }
 
-  protected open fun includeRecord(fieldExtractor: FieldExtractor): TryEither<Boolean> =
-      Either.Right(true)
-
   protected abstract fun getTransaction(
       userId: Long,
       fieldExtractor: FieldExtractor
   ): TryEither<Transaction>
-
-  private data class FilteredFieldExtractor(
-      val fieldExtractor: FieldExtractor,
-      val include: Boolean
-  )
 }
