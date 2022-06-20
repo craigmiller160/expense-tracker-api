aa8ac7df0591e87d0297e32f44360972477b6289
Craig Miller <craigmiller160@gmail.com>
2022-06-20 13:19:57 -0400

    More work on TransactionResponse

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/TransactionResponse.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/TransactionResponse.kt
@@ -3,6 +3,8 @@ package io.craigmiller160.expensetrackerapi.web.types
 import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.CategoryId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.TransactionId
+import io.craigmiller160.expensetrackerapi.data.model.Category
+import io.craigmiller160.expensetrackerapi.data.model.Transaction
 import java.math.BigDecimal
 import java.time.LocalDate
 
@@ -14,4 +16,16 @@ data class TransactionResponse(
     val confirmed: Boolean,
     val categoryId: TypedId<CategoryId>?,
     val categoryName: String?
-)
+) {
+  companion object {
+    fun from(transaction: Transaction, category: Category?): TransactionResponse =
+        TransactionResponse(
+            id = transaction.id,
+            expenseDate = transaction.expenseDate,
+            description = transaction.description,
+            amount = transaction.amount,
+            confirmed = transaction.confirmed,
+            categoryId = category?.id,
+            categoryName = category?.name)
+  }
+}
