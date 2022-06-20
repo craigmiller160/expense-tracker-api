cf737ad57e7d14f3ed7fb92e6ab3e5484d0f705c
Craig Miller <craigmiller160@gmail.com>
2022-06-20 13:39:34 -0400

    More request tweaks

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/TransactionSearchRequest.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/TransactionSearchRequest.kt
@@ -3,9 +3,9 @@ package io.craigmiller160.expensetrackerapi.web.types
 import java.time.LocalDate
 
 data class TransactionSearchRequest(
-    val startDate: LocalDate,
-    val endDate: LocalDate,
-    val confirmed: Boolean,
+    val startDate: LocalDate?,
+    val endDate: LocalDate?,
+    val confirmed: Boolean?,
     override val pageNumber: Int,
     override val pageSize: Int
 ) : PageableRequest
