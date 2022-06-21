e7b599d8c80b95dca34cd6595ac337e7c59f4010
Craig Miller <craigmiller160@gmail.com>
2022-06-21 09:35:12 -0400

    More improvements to categorize flow

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/service/TransactionService.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/service/TransactionService.kt
@@ -1,13 +1,13 @@
 package io.craigmiller160.expensetrackerapi.service
 
 import arrow.core.Either
-import arrow.core.sequence
-import io.craigmiller160.expensetrackerapi.data.repository.CategoryRepository
 import io.craigmiller160.expensetrackerapi.data.repository.TransactionRepository
 import io.craigmiller160.expensetrackerapi.function.TryEither
+import io.craigmiller160.expensetrackerapi.function.flatMapCatch
 import io.craigmiller160.expensetrackerapi.web.types.CategorizeTransactionsRequest
 import io.craigmiller160.expensetrackerapi.web.types.DeleteTransactionsRequest
 import io.craigmiller160.expensetrackerapi.web.types.SearchTransactionsRequest
+import io.craigmiller160.expensetrackerapi.web.types.TransactionAndCategory
 import io.craigmiller160.expensetrackerapi.web.types.TransactionResponse
 import io.craigmiller160.oauth2.service.OAuth2Service
 import org.springframework.stereotype.Service
@@ -16,20 +16,18 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class TransactionService(
     private val transactionRepository: TransactionRepository,
-    private val categoryRepository: CategoryRepository,
     private val oAuth2Service: OAuth2Service
 ) {
   @Transactional
-  fun categorizeTransactions(request: CategorizeTransactionsRequest): TryEither<List<Unit>> {
+  fun categorizeTransactions(request: CategorizeTransactionsRequest): TryEither<Unit> {
     val userId = oAuth2Service.getAuthenticatedUser().userId
-    return request.transactionsAndCategories
-        .map { txnAndCat ->
-          Either.catch {
-            transactionRepository.setTransactionCategory(
-                txnAndCat.transactionId, txnAndCat.categoryId, userId)
-          }
-        }
-        .sequence()
+    return request.transactionsAndCategories.foldRight<TransactionAndCategory, TryEither<Unit>>(
+        Either.Right(Unit)) { txnAndCat, result ->
+      result.flatMapCatch {
+        transactionRepository.setTransactionCategory(
+            txnAndCat.transactionId, txnAndCat.categoryId, userId)
+      }
+    }
   }
 
   @Transactional
