113cff69db96f012a56d3a8cef41b13d4fe26824
Craig Miller <craigmiller160@gmail.com>
2022-06-21 12:02:59 -0400

    Progress with search logic

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/service/TransactionService.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/service/TransactionService.kt
@@ -1,6 +1,10 @@
 package io.craigmiller160.expensetrackerapi.service
 
 import arrow.core.Either
+import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
+import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.CategoryId
+import io.craigmiller160.expensetrackerapi.data.model.Category
+import io.craigmiller160.expensetrackerapi.data.repository.CategoryRepository
 import io.craigmiller160.expensetrackerapi.data.repository.TransactionRepository
 import io.craigmiller160.expensetrackerapi.function.TryEither
 import io.craigmiller160.expensetrackerapi.function.flatMapCatch
@@ -10,12 +14,14 @@ import io.craigmiller160.expensetrackerapi.web.types.SearchTransactionsRequest
 import io.craigmiller160.expensetrackerapi.web.types.SearchTransactionsResponse
 import io.craigmiller160.expensetrackerapi.web.types.TransactionAndCategory
 import io.craigmiller160.oauth2.service.OAuth2Service
+import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class TransactionService(
     private val transactionRepository: TransactionRepository,
+    private val categoryRepository: CategoryRepository,
     private val oAuth2Service: OAuth2Service
 ) {
   @Transactional
@@ -36,7 +42,22 @@ class TransactionService(
     return Either.catch { transactionRepository.deleteTransactions(request.ids, userId) }
   }
 
+  @Transactional
   fun search(request: SearchTransactionsRequest): TryEither<SearchTransactionsResponse> {
-    TODO()
+    val userId = oAuth2Service.getAuthenticatedUser().userId
+    val categories = getCategoryMap(userId)
+    val pageable = PageRequest.of(request.pageNumber, request.pageSize)
+    val page =
+        transactionRepository.searchTransactions(
+            userId,
+            request.startDate,
+            request.endDate,
+            request.confirmed,
+            request.categoryIds,
+            pageable)
+    return SearchTransactionsResponse.from(page, categories)
   }
+
+  private fun getCategoryMap(userId: Long): Map<TypedId<CategoryId>, Category> =
+      categoryRepository.findAllByUserId(userId).associateBy { it.id }
 }
--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/SearchTransactionsResponse.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/SearchTransactionsResponse.kt
@@ -1,7 +1,22 @@
 package io.craigmiller160.expensetrackerapi.web.types
 
+import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
+import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.CategoryId
+import io.craigmiller160.expensetrackerapi.data.model.Category
+import io.craigmiller160.expensetrackerapi.data.model.Transaction
+import org.springframework.data.domain.Page
+
 data class SearchTransactionsResponse(
     val transactions: List<TransactionResponse>,
     val pageNumber: Int,
     val totalItems: Long
-)
+) {
+  companion object {
+    fun from(
+        page: Page<Transaction>,
+        categories: Map<TypedId<CategoryId>, Category>
+    ): SearchTransactionsResponse {
+      TODO()
+    }
+  }
+}
