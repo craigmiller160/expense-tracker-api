d0805e2c35883825912b2e9946ee2f86e3b15381
Craig Miller <craigmiller160@gmail.com>
2022-06-21 11:57:35 -0400

    Working on search query

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/data/repository/TransactionRepository.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/repository/TransactionRepository.kt
@@ -4,6 +4,9 @@ import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.CategoryId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.TransactionId
 import io.craigmiller160.expensetrackerapi.data.model.Transaction
+import java.time.LocalDate
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
@@ -45,4 +48,19 @@ interface TransactionRepository : JpaRepository<Transaction, TypedId<Transaction
       @Param("transactionIds") transactionIds: Set<TypedId<TransactionId>>,
       @Param("userId") userId: Long
   )
+
+  @Query(
+      """
+      SELECT t.*
+      FROM Transaction t
+      WHERE (:startDate IS NULL OR t.expenseDate >= :startDate)
+      AND (:endDate IS NULL OR t.expenseDate <= :endDate)
+      AND (:confirmed IS NULL OR t.confirmed = :confirmed)
+  """)
+  fun searchTransactions(
+      @Param("startDate") startDate: LocalDate?,
+      @Param("endDate") endDate: LocalDate?,
+      @Param("confirmed") confirmed: Boolean?,
+      page: Pageable
+  ): Page<Transaction>
 }
