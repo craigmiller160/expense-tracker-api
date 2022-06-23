64cbf3acdbf2975d7d7b6fa6a1674a118871088d
Craig Miller <craigmiller160@gmail.com>
2022-06-23 10:23:08 -0400

    Code cleanup

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/common/data/typedid/spring/TypedIdConverter.kt
+++
@@ -1,8 +0,0 @@
-package io.craigmiller160.expensetrackerapi.common.data.typedid.spring
-
-import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
-import org.springframework.core.convert.converter.Converter
-
-class TypedIdConverter : Converter<String, TypedId<*>> {
-  override fun convert(source: String): TypedId<*> = TypedId<Any>(source)
-}
