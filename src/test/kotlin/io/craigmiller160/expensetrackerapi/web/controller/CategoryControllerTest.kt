6cccf98a0ee8320efab9d4dbba3b717a1f9b3948
Craig Miller <craigmiller160@gmail.com>
2022-06-23 11:52:48 -0400

    Stubbed out new tests

---
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/web/controller/CategoryControllerTest.kt
@@ -0,0 +1,26 @@
+package io.craigmiller160.expensetrackerapi.web.controller
+
+import io.craigmiller160.expensetrackerapi.BaseIntegrationTest
+import org.junit.jupiter.api.Test
+
+class CategoryControllerTest : BaseIntegrationTest() {
+  @Test
+  fun getAllCategories() {
+    TODO()
+  }
+
+  @Test
+  fun createCategory() {
+    TODO()
+  }
+
+  @Test
+  fun updateCategory() {
+    TODO()
+  }
+
+  @Test
+  fun deleteCategory() {
+    TODO()
+  }
+}
