dfb84fba9c53dd1f641415e19fb86da62e3b7f4b
Craig Miller <craigmiller160@gmail.com>
2022-06-30 16:21:52 -0400

    Improved error handling

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/ControllerErrorHandler.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/ControllerErrorHandler.kt
@@ -4,6 +4,7 @@ import io.craigmiller160.expensetrackerapi.common.error.BadRequestException
 import io.craigmiller160.expensetrackerapi.web.types.ErrorResponse
 import java.time.ZoneId
 import java.time.ZonedDateTime
+import javax.transaction.InvalidTransactionException
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.AccessDeniedException
@@ -24,6 +25,12 @@ class ControllerErrorHandler {
 
   @ExceptionHandler(BadRequestException::class)
   fun badRequestException(ex: BadRequestException): ResponseEntity<ErrorResponse> {
+    log.error(ex.message, ex)
+    return createErrorResponse(400, ex.message ?: "")
+  }
+
+  @ExceptionHandler(InvalidTransactionException::class)
+  fun invalidTransactionException(ex: InvalidTransactionException): ResponseEntity<ErrorResponse> {
     log.error(ex.message, ex)
     return createErrorResponse(400, ex.message ?: "")
   }
