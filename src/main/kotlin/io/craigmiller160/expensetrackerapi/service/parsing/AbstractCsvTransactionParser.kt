45740e6f859a75de658274bd9ccf40103f15eb4d
Craig Miller <craigmiller160@gmail.com>
2022-06-30 14:28:00 -0400

    Integrated column check into CSV parsing

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/service/parsing/AbstractCsvTransactionParser.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/service/parsing/AbstractCsvTransactionParser.kt
@@ -1,6 +1,7 @@
 package io.craigmiller160.expensetrackerapi.service.parsing
 
 import arrow.core.Either
+import arrow.core.flatMap
 import arrow.core.sequence
 import com.opencsv.CSVReader
 import io.craigmiller160.expensetrackerapi.common.error.InvalidImportException
@@ -22,16 +23,23 @@ abstract class AbstractCsvTransactionParser : TransactionParser {
           .drop(1)
           .map { prepareFieldExtractor(it) }
           .mapIndexed { index, fieldExtractor ->
-            getTransaction(userId, fieldExtractor).mapLeft {
-              InvalidImportException("Error parsing CSV record, row ${index + 2}", it)
+            fieldExtractor.flatMap {
+              getTransaction(userId, it).mapLeft {
+                InvalidImportException("Error parsing CSV record, row ${index + 2}", it)
+              }
             }
           }
           .filter { either -> either.exists { includeTransaction(it) } }
           .sequence()
 
-  private fun prepareFieldExtractor(fields: Array<String>): FieldExtractor = { index, name ->
-    Either.catch { fields[index] }
-        .mapLeft { InvalidImportException("Missing field $name at CSV row index $index") }
+  private fun prepareFieldExtractor(fields: Array<String>): TryEither<FieldExtractor> {
+    if (fields.size != numberOfColumns) {
+      return Either.Left(InvalidImportException("Invalid number of columns in CSV: ${fields.size}"))
+    }
+    return Either.Right { index, name ->
+      Either.catch { fields[index] }
+          .mapLeft { InvalidImportException("Missing field $name at CSV row index $index") }
+    }
   }
 
   protected open fun includeTransaction(transaction: Transaction): Boolean = true
