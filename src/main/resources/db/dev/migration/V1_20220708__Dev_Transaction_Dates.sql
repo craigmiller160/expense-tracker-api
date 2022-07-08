6bda220c1c4e002782b6890961480684bad7e0ca
Craig Miller <craigmiller160@gmail.com>
2022-07-08 11:01:27 -0400

    First draft of procedure

--- src/main/resources/db/dev/migration/V1_20220708__Dev_Transaction_Dates.sql
+++ src/main/resources/db/dev/migration/V1_20220708__Dev_Transaction_Dates.sql
@@ -3,8 +3,16 @@ AS
 $$
     DECLARE
         the_date DATE;
+        the_row transactions%rowtype;
     BEGIN
+        the_date = now();
+        FOR the_row IN SELECT * FROM transactions LOOP
+            UPDATE transactions
+            SET expense_date = the_date
+            WHERE id = the_row.id;
 
+            the_date = the_date - 1;
+        END LOOP;
     END;
 $$
 LANGUAGE plpgsql;
