618384748ec803594df852f8d59a3400ac4eae70
Craig Miller <craigmiller160@gmail.com>
2022-07-08 11:44:37 -0400

    Stubbed out new endpoint

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/controller/TransactionController.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/controller/TransactionController.kt
@@ -3,17 +3,20 @@ package io.craigmiller160.expensetrackerapi.web.controller
 import io.craigmiller160.expensetrackerapi.function.TryEither
 import io.craigmiller160.expensetrackerapi.service.TransactionService
 import io.craigmiller160.expensetrackerapi.web.types.CategorizeTransactionsRequest
+import io.craigmiller160.expensetrackerapi.web.types.ConfirmTransactionsRequest
 import io.craigmiller160.expensetrackerapi.web.types.DeleteTransactionsRequest
 import io.craigmiller160.expensetrackerapi.web.types.NeedsAttentionResponse
 import io.craigmiller160.expensetrackerapi.web.types.SearchTransactionsRequest
 import io.craigmiller160.expensetrackerapi.web.types.SearchTransactionsResponse
 import javax.validation.Valid
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -30,10 +33,14 @@ class TransactionController(private val transactionService: TransactionService) 
       transactionService.deleteTransactions(request).map { ResponseEntity.noContent().build() }
 
   @PutMapping("/categorize")
-  fun categorizeTransactions(
-      @RequestBody request: CategorizeTransactionsRequest
-  ): TryEither<ResponseEntity<Unit>> =
-      transactionService.categorizeTransactions(request).map { ResponseEntity.noContent().build() }
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  fun categorizeTransactions(@RequestBody request: CategorizeTransactionsRequest): TryEither<Unit> =
+      transactionService.categorizeTransactions(request)
+
+  @PutMapping("/confirm")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  fun confirmTransactions(@RequestBody request: ConfirmTransactionsRequest): TryEither<Unit> =
+      TODO()
 
   @GetMapping("/needs-attention")
   fun getNeedsAttention(): TryEither<NeedsAttentionResponse> =
