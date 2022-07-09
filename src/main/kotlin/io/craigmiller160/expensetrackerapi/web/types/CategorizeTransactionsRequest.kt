133284876f295e66e27b78b1b64e4bd30aff667b
Craig Miller <craigmiller160@gmail.com>
2022-07-09 11:46:33 -0400

    Reverting some changes to categorizing transactions

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/CategorizeTransactionsRequest.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/CategorizeTransactionsRequest.kt
@@ -1,5 +1,14 @@
 package io.craigmiller160.expensetrackerapi.web.types
 
+import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
+import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.CategoryId
+import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.TransactionId
+
+data class TransactionAndCategory(
+    val transactionId: TypedId<TransactionId>,
+    val categoryId: TypedId<CategoryId>?
+)
+
 data class CategorizeTransactionsRequest(
     val transactionsAndCategories: Set<TransactionAndCategory>
 )
