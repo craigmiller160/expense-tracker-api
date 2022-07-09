02ec1649ff6f50de474a7b9a79570a32a58dc452
Craig Miller <craigmiller160@gmail.com>
2022-07-09 11:51:19 -0400

    Refactored update request

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/UpdateTransactionsRequest.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/UpdateTransactionsRequest.kt
@@ -1,9 +1,13 @@
 package io.craigmiller160.expensetrackerapi.web.types
 
 import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
+import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.CategoryId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.TransactionId
 
-data class UpdateTransactionsRequest(
-    val categorize: Set<TransactionAndCategory>,
-    val confirm: Set<TypedId<TransactionId>>
+data class TransactionToUpdate(
+    val transactionId: TypedId<TransactionId>,
+    val categoryId: TypedId<CategoryId>?,
+    val confirmed: Boolean
 )
+
+data class UpdateTransactionsRequest(val transactions: Set<TransactionToUpdate>)
