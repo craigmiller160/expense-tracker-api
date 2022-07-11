8203b8c113fdace89a6b871263dd123af179ea4e
Craig Miller <craigmiller160@gmail.com>
2022-07-11 07:38:20 -0400

    Refactored controller endpoint

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/controller/TransactionController.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/controller/TransactionController.kt
@@ -11,7 +11,6 @@ import io.craigmiller160.expensetrackerapi.web.types.SearchTransactionsResponse
 import io.craigmiller160.expensetrackerapi.web.types.UpdateTransactionsRequest
 import javax.validation.Valid
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PutMapping
@@ -28,10 +27,9 @@ class TransactionController(private val transactionService: TransactionService) 
       transactionService.search(request)
 
   @DeleteMapping
-  fun deleteTransactions(
-      @RequestBody request: DeleteTransactionsRequest
-  ): TryEither<ResponseEntity<Unit>> =
-      transactionService.deleteTransactions(request).map { ResponseEntity.noContent().build() }
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  fun deleteTransactions(@RequestBody request: DeleteTransactionsRequest): TryEither<Unit> =
+      transactionService.deleteTransactions(request)
 
   @PutMapping("/categorize")
   @ResponseStatus(HttpStatus.NO_CONTENT)
