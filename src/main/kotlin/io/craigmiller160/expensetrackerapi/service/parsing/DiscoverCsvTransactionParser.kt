54cd5b80117abb61cbd74651a66d5de0def3ee8f
Craig Miller <craigmiller160@gmail.com>
2022-09-29 11:25:46 -0400

    More csv parser refactoring

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/service/parsing/DiscoverCsvTransactionParser.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/service/parsing/DiscoverCsvTransactionParser.kt
@@ -1,33 +1,34 @@
 package io.craigmiller160.expensetrackerapi.service.parsing
 
-import arrow.core.Either
-import arrow.core.continuations.either
 import io.craigmiller160.expensetrackerapi.data.model.Transaction
 import io.craigmiller160.expensetrackerapi.function.TryEither
-import java.math.BigDecimal
-import java.time.LocalDate
+import java.io.InputStream
 import java.time.format.DateTimeFormatter
 import org.springframework.stereotype.Component
 
 @Component
-class DiscoverCsvTransactionParser : AbstractCsvTransactionParser() {
+class DiscoverCsvTransactionParser : TransactionParser {
   companion object {
     private val DATE_FORMAT = DateTimeFormatter.ofPattern("MM/dd/yyyy")
   }
 
-  override val numberOfColumns: Int = 5
+  //  val numberOfColumns: Int = 5
+  //
+  //  fun getTransaction(
+  //    userId: Long,
+  //    fieldExtractor: FieldExtractor
+  //  ): TryEither<Transaction> =
+  //    either.eager {
+  //      val transactionDate = fieldExtractor(0, "transactionDate").bind()
+  //      val expenseDate = Either.catch { LocalDate.parse(transactionDate, DATE_FORMAT) }.bind()
+  //      val description = fieldExtractor(2, "description").bind()
+  //      val rawAmount = fieldExtractor(3, "amount").bind()
+  //      val amount = Either.catch { BigDecimal(rawAmount) }.bind()
+  //      Transaction(
+  //        userId = userId, expenseDate = expenseDate, description = description, amount = amount)
+  //    }
 
-  override fun getTransaction(
-    userId: Long,
-    fieldExtractor: FieldExtractor
-  ): TryEither<Transaction> =
-    either.eager {
-      val transactionDate = fieldExtractor(0, "transactionDate").bind()
-      val expenseDate = Either.catch { LocalDate.parse(transactionDate, DATE_FORMAT) }.bind()
-      val description = fieldExtractor(2, "description").bind()
-      val rawAmount = fieldExtractor(3, "amount").bind()
-      val amount = Either.catch { BigDecimal(rawAmount) }.bind()
-      Transaction(
-        userId = userId, expenseDate = expenseDate, description = description, amount = amount)
-    }
+  override fun parse(userId: Long, stream: InputStream): TryEither<List<Transaction>> {
+    TODO("Not yet implemented")
+  }
 }
