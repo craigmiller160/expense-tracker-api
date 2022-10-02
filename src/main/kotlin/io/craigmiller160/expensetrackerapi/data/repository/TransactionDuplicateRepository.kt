51385d08aecb0fc11d80f58d5f1b78f26bd641e9
Craig Miller <craigmiller160@gmail.com>
2022-10-02 12:37:40 -0400

    Added new repo

---
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/repository/TransactionDuplicateRepository.kt
@@ -0,0 +1,9 @@
+package io.craigmiller160.expensetrackerapi.data.repository
+
+import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
+import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.TransactionDuplicateId
+import io.craigmiller160.expensetrackerapi.data.model.TransactionDuplicate
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface TransactionDuplicateRepository :
+  JpaRepository<TransactionDuplicate, TypedId<TransactionDuplicateId>>
