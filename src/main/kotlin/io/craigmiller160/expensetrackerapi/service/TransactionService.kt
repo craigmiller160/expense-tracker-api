d27cbf0258dbcf28dfa1880f82a75621723c6e7d
Craig Miller <craigmiller160@gmail.com>
2022-10-14 15:39:29 -0400

    Added logic

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/service/TransactionService.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/service/TransactionService.kt
@@ -194,7 +194,8 @@ class TransactionService(
     }
 
   @Transactional
-  fun markNotDuplicate(transactionId: TypedId<TransactionId>): TryEither<Unit> = TODO()
+  fun markNotDuplicate(transactionId: TypedId<TransactionId>): TryEither<Unit> =
+    Either.catch { transactionRepository.markNotDuplicate(System.nanoTime(), transactionId) }
 
   private fun getCategoryMap(userId: Long): TryEither<Map<TypedId<CategoryId>, Category>> =
     Either.catch { categoryRepository.findAllByUserIdOrderByName(userId).associateBy { it.id } }
