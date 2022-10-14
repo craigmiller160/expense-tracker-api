b9bd40154ec3266d4b59bf9a8f96e9c6f03e8edb
Craig Miller <craigmiller160@gmail.com>
2022-10-14 15:15:47 -0400

    Setup new query

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/data/repository/TransactionViewRepository.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/repository/TransactionViewRepository.kt
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -15,15 +16,17 @@ interface TransactionViewRepository : JpaRepository<TransactionView, TypedId<Tra
 
   @Query(
     """
-    SELECT t
-    FROM TransactionView t
-    WHERE t.id IN (
-        SELECT t
-        
+    SELECT t1
+    FROM TransactionView t1
+    WHERE t1.contentHash IN (
+        SELECT t2.contentHash
+        FROM TransactionView t2
+        WHERE t2.id = :transactionId
     )
+    AND t1.id <> :transactionId
   """)
   fun findAllDuplicates(
-    transactionId: TypedId<TransactionId>,
+    @Param("transactionId") transactionId: TypedId<TransactionId>,
     page: Pageable
   ): Page<TransactionView>
 }
