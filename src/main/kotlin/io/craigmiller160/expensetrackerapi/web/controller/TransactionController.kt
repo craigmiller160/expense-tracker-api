681154978a2efd35e1e2e730c2f9e1f170acdc0b
Craig Miller <craigmiller160@gmail.com>
2022-10-16 22:42:46 -0400

    More attempts to solve the problem

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/controller/TransactionController.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/controller/TransactionController.kt
@@ -16,9 +16,6 @@ import io.craigmiller160.expensetrackerapi.web.types.TransactionResponse
 import io.craigmiller160.expensetrackerapi.web.types.TransactionsPageResponse
 import io.craigmiller160.expensetrackerapi.web.types.UpdateTransactionDetailsRequest
 import io.craigmiller160.expensetrackerapi.web.types.UpdateTransactionsRequest
-import io.swagger.v3.oas.annotations.media.Content
-import io.swagger.v3.oas.annotations.media.Schema
-import io.swagger.v3.oas.annotations.responses.ApiResponse
 import javax.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -35,12 +32,12 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/transactions")
 class TransactionController(private val transactionService: TransactionService) {
   // TODO need better solution than this
-  @ApiResponse(
-    content =
-      [
-        Content(
-          mediaType = "application/json",
-          schema = Schema(implementation = TransactionsPageResponse::class))])
+  //  @ApiResponse(
+  //    content =
+  //      [
+  //        Content(
+  //          mediaType = "application/json",
+  //          schema = Schema(implementation = TransactionsPageResponse::class))])
   @GetMapping
   fun search(@Valid request: SearchTransactionsRequest): TryEither<TransactionsPageResponse> =
     transactionService.search(request)
