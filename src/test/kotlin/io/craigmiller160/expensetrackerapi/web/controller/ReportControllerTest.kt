c6718bace82c180bd9d568df13d9f497781d5708
Craig Miller <craigmiller160@gmail.com>
2022-11-10 13:38:53 -0500

    Tweaks

--- src/test/kotlin/io/craigmiller160/expensetrackerapi/web/controller/ReportControllerTest.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/web/controller/ReportControllerTest.kt
@@ -2,6 +2,7 @@ package io.craigmiller160.expensetrackerapi.web.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.craigmiller160.expensetrackerapi.testcore.ExpenseTrackerIntegrationTest
+import io.craigmiller160.expensetrackerapi.testutils.DataHelper
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.web.servlet.MockMvc
@@ -9,7 +10,11 @@ import org.springframework.test.web.servlet.MockMvc
 @ExpenseTrackerIntegrationTest
 class ReportControllerTest
 @Autowired
-constructor(private val mockMvc: MockMvc, private val objectMapper: ObjectMapper) {
+constructor(
+  private val mockMvc: MockMvc,
+  private val objectMapper: ObjectMapper,
+  private val dataHelper: DataHelper
+) {
   @Test
   fun getReports() {
     TODO()
