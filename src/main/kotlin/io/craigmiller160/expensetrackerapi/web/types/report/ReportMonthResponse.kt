91233babf55511dd9cd68e486052433c3d56053f
Craig Miller <craigmiller160@gmail.com>
2022-11-11 11:38:23 -0500

    Cleanup

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/report/ReportMonthResponse.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/report/ReportMonthResponse.kt
@@ -5,7 +5,7 @@ import java.math.BigDecimal
 import java.time.LocalDate
 
 data class ReportMonthResponse(
-  val date: LocalDate, // TODO can I make this a month instead?
+  val date: LocalDate,
   val total: BigDecimal,
   val categories: List<ReportCategoryResponse>
 ) {
