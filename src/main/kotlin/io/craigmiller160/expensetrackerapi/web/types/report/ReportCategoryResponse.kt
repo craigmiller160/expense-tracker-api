2ef821c6bee5892dbb625d6b43cb6cf7493de106
Craig Miller <craigmiller160@gmail.com>
2022-11-11 09:18:36 -0500

    Cleanup

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/report/ReportCategoryResponse.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/report/ReportCategoryResponse.kt
@@ -1,12 +1,9 @@
 package io.craigmiller160.expensetrackerapi.web.types.report
 
-import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
-import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.CategoryId
 import io.craigmiller160.expensetrackerapi.data.projection.SpendingByCategory
 import java.math.BigDecimal
 
 data class ReportCategoryResponse(
-  val id: TypedId<CategoryId>,
   val name: String,
   val amount: BigDecimal,
   val percent: BigDecimal
@@ -14,7 +11,6 @@ data class ReportCategoryResponse(
   companion object {
     fun from(category: SpendingByCategory, monthTotal: BigDecimal): ReportCategoryResponse =
       ReportCategoryResponse(
-        id = TODO(),
         name = category.categoryName,
         amount = category.total,
         percent = category.total / monthTotal)
