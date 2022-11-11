773e59a3ece98ba377a14ff41400986c925fd42f
Craig Miller <craigmiller160@gmail.com>
2022-11-11 12:31:48 -0500

    Added a new migration

--- src/main/kotlin/db/dev/migration/V1_20221110__Set_Category_Colors.kt
+++ src/main/kotlin/db/dev/migration/V1_20221110__Set_Category_Colors.kt
@@ -1,6 +1,5 @@
 package db.dev.migration
 
-import io.craigmiller160.expensetrackerapi.utils.StringToColor
 import java.util.UUID
 import org.flywaydb.core.api.migration.BaseJavaMigration
 import org.flywaydb.core.api.migration.Context
@@ -28,17 +27,20 @@ class V1_20221110__Set_Category_Colors : BaseJavaMigration() {
     val jdbcTemplate =
       NamedParameterJdbcTemplate(SingleConnectionDataSource(context.connection, true))
     jdbcTemplate
-      .query(GET_CATEGORY_IDS, MapSqlParameterSource()) { rs, i ->
+      .query(GET_CATEGORY_IDS, MapSqlParameterSource()) { rs, _ ->
         CategoryRecord(id = UUID.fromString(rs.getString("id")), name = rs.getString("name"))
       }
       .forEach { category ->
         val params =
           MapSqlParameterSource()
             .addValue("id", category.id)
-            .addValue("color", StringToColor.get(category.name))
+            .addValue("color", stringToColor(category.name))
         jdbcTemplate.update(UPDATE_COLORS, params)
       }
   }
 
   private data class CategoryRecord(val id: UUID, val name: String)
+
+  private fun stringToColor(value: String): String =
+    String.format("#%06x", 0xFFFFFF and value.hashCode())
 }
---
+++ src/main/kotlin/db/dev/migration/V1_20221111__Mass_Add_Categories.kt
@@ -0,0 +1,69 @@
+package db.dev.migration
+
+import java.util.UUID
+import org.flywaydb.core.api.migration.BaseJavaMigration
+import org.flywaydb.core.api.migration.Context
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.springframework.jdbc.datasource.SingleConnectionDataSource
+
+class V1_20221111__Mass_Add_Categories : BaseJavaMigration() {
+  companion object {
+    private const val GET_CATEGORY_IDS =
+      """
+            SELECT id
+            FROM categories
+        """
+
+    private const val GET_HALF_TRANSACTIONS =
+      """
+            SELECT id
+            FROM transactions
+            LIMIT (
+                SELECT COUNT(*) / 2
+                FROM transactions
+            )
+        """
+
+    private const val SET_CATEGORY_ON_TRANSACTIONS =
+      """
+            UPDATE transactions
+            SET category_id = :categoryId
+            WHERE id = :transactionId
+        """
+  }
+
+  override fun migrate(context: Context) {
+    val jdbcTemplate =
+      NamedParameterJdbcTemplate(SingleConnectionDataSource(context.connection, true))
+    val categoryIds = getCategoryIds(jdbcTemplate)
+    val transactionIds = getTransactionIds(jdbcTemplate)
+    setCategoryOnTransaction(jdbcTemplate, transactionIds, categoryIds)
+  }
+
+  private fun getTransactionIds(jdbcTemplate: NamedParameterJdbcTemplate): List<UUID> =
+    jdbcTemplate.query(GET_HALF_TRANSACTIONS, MapSqlParameterSource()) { rs, _ ->
+      UUID.fromString(rs.getString("id"))
+    }
+
+  private fun setCategoryOnTransaction(
+    jdbcTemplate: NamedParameterJdbcTemplate,
+    transactionIds: List<UUID>,
+    categoryIds: List<UUID>
+  ) {
+    val batchParams =
+      transactionIds
+        .mapIndexed { index, transactionId ->
+          MapSqlParameterSource()
+            .addValue("transactionId", transactionId)
+            .addValue("categoryId", categoryIds[categoryIds.size % index])
+        }
+        .toTypedArray()
+    jdbcTemplate.batchUpdate(SET_CATEGORY_ON_TRANSACTIONS, batchParams)
+  }
+
+  private fun getCategoryIds(jdbcTemplate: NamedParameterJdbcTemplate): List<UUID> =
+    jdbcTemplate.query(GET_CATEGORY_IDS, MapSqlParameterSource()) { rs, _ ->
+      UUID.fromString(rs.getString("id"))
+    }
+}
