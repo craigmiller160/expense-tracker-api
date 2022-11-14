7fe220b65acf02dcaba580eede3162b55d1fd9ae
Craig Miller <craigmiller160@gmail.com>
2022-11-14 11:42:40 -0500

    Finished implementing new service

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/service/NeedsAttentionService.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/service/NeedsAttentionService.kt
@@ -2,22 +2,27 @@ package io.craigmiller160.expensetrackerapi.service
 
 import arrow.core.Either
 import io.craigmiller160.expensetrackerapi.data.projection.NeedsAttentionType
+import io.craigmiller160.expensetrackerapi.data.repository.NeedsAttentionRepository
 import io.craigmiller160.expensetrackerapi.function.TryEither
 import io.craigmiller160.expensetrackerapi.web.types.CountAndOldest
 import io.craigmiller160.expensetrackerapi.web.types.NeedsAttentionResponse
+import io.craigmiller160.oauth2.service.OAuth2Service
 import javax.transaction.Transactional
 import org.springframework.stereotype.Service
 
 @Service
-class NeedsAttentionService {
+class NeedsAttentionService(
+  private val needsAttentionRepository: NeedsAttentionRepository,
+  private val oAuth2Service: OAuth2Service
+) {
   @org.springframework.transaction.annotation.Transactional
   fun getNeedsAttention(): TryEither<NeedsAttentionResponse> {
     val userId = oAuth2Service.getAuthenticatedUser().userId
     return Either.catch {
       val needsAttentionCounts =
-        transactionRepository.getAllNeedsAttentionCounts(userId).associateBy { it.type }
+        needsAttentionRepository.getAllNeedsAttentionCounts(userId).associateBy { it.type }
       val needsAttentionOldest =
-        transactionRepository.getAllNeedsAttentionOldest(userId).associateBy { it.type }
+        needsAttentionRepository.getAllNeedsAttentionOldest(userId).associateBy { it.type }
 
       NeedsAttentionResponse(
         unconfirmed =
