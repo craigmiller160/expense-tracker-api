ecded9c04faad691101ec5fb5abc77a710e93c97
Craig Miller <craigmiller160@gmail.com>
2022-11-14 11:42:16 -0500

    Added needs attention repo

---
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/repository/impl/NeedsAttentionRepositoryImpl.kt
@@ -0,0 +1,39 @@
+package io.craigmiller160.expensetrackerapi.data.repository.impl
+
+import io.craigmiller160.expensetrackerapi.data.SqlLoader
+import io.craigmiller160.expensetrackerapi.data.projection.NeedsAttentionCount
+import io.craigmiller160.expensetrackerapi.data.projection.NeedsAttentionOldest
+import io.craigmiller160.expensetrackerapi.data.projection.NeedsAttentionType
+import io.craigmiller160.expensetrackerapi.data.repository.NeedsAttentionRepository
+import org.springframework.jdbc.core.RowMapper
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.springframework.stereotype.Repository
+
+private val needsAttentionCountRowMapper: RowMapper<NeedsAttentionCount> = RowMapper { rs, index ->
+  NeedsAttentionCount(NeedsAttentionType.valueOf(rs.getString("type")), rs.getLong("count"))
+}
+
+private val needsAttentionOldestRowMapper: RowMapper<NeedsAttentionOldest> =
+  RowMapper { rs, index ->
+    NeedsAttentionOldest(
+      NeedsAttentionType.valueOf(rs.getString("type")), rs.getDate("oldest")?.toLocalDate())
+  }
+
+@Repository
+class NeedsAttentionRepositoryImpl(
+  private val jdbcTemplate: NamedParameterJdbcTemplate,
+  private val sqlLoader: SqlLoader
+) : NeedsAttentionRepository {
+  override fun getAllNeedsAttentionCounts(userId: Long): List<NeedsAttentionCount> {
+    val countSql = sqlLoader.loadSql("needsAttention/get_all_needs_attention_counts.sql")
+    val params = MapSqlParameterSource().addValue("userId", userId)
+    return jdbcTemplate.query(countSql, params, needsAttentionCountRowMapper)
+  }
+
+  override fun getAllNeedsAttentionOldest(userId: Long): List<NeedsAttentionOldest> {
+    val oldestSql = sqlLoader.loadSql("needsAttention/get_all_needs_attention_oldest.sql")
+    val params = MapSqlParameterSource().addValue("userId", userId)
+    return jdbcTemplate.query(oldestSql, params, needsAttentionOldestRowMapper)
+  }
+}
--- src/main/kotlin/io/craigmiller160/expensetrackerapi/data/repository/impl/TransactionRepositoryCustomImpl.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/repository/impl/TransactionRepositoryCustomImpl.kt
@@ -2,52 +2,21 @@ package io.craigmiller160.expensetrackerapi.data.repository.impl
 
 import com.querydsl.core.BooleanBuilder
 import com.querydsl.jpa.impl.JPAQueryFactory
-import io.craigmiller160.expensetrackerapi.data.SqlLoader
 import io.craigmiller160.expensetrackerapi.data.model.QTransactionView
 import io.craigmiller160.expensetrackerapi.data.model.TransactionView
-import io.craigmiller160.expensetrackerapi.data.projection.NeedsAttentionCount
-import io.craigmiller160.expensetrackerapi.data.projection.NeedsAttentionOldest
-import io.craigmiller160.expensetrackerapi.data.projection.NeedsAttentionType
 import io.craigmiller160.expensetrackerapi.data.querydsl.QueryDSLSupport
 import io.craigmiller160.expensetrackerapi.data.repository.TransactionRepositoryCustom
 import io.craigmiller160.expensetrackerapi.web.types.transaction.SearchTransactionsRequest
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
-import org.springframework.jdbc.core.RowMapper
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Repository
-
-private val needsAttentionCountRowMapper: RowMapper<NeedsAttentionCount> = RowMapper { rs, index ->
-  NeedsAttentionCount(NeedsAttentionType.valueOf(rs.getString("type")), rs.getLong("count"))
-}
-
-private val needsAttentionOldestRowMapper: RowMapper<NeedsAttentionOldest> =
-  RowMapper { rs, index ->
-    NeedsAttentionOldest(
-      NeedsAttentionType.valueOf(rs.getString("type")), rs.getDate("oldest")?.toLocalDate())
-  }
 
 @Repository
 class TransactionRepositoryCustomImpl(
   private val queryFactory: JPAQueryFactory,
-  private val queryDslSupport: QueryDSLSupport,
-  private val jdbcTemplate: NamedParameterJdbcTemplate,
-  private val sqlLoader: SqlLoader
+  private val queryDslSupport: QueryDSLSupport
 ) : TransactionRepositoryCustom {
-
-  override fun getAllNeedsAttentionCounts(userId: Long): List<NeedsAttentionCount> {
-    val countSql = sqlLoader.loadSql("needsAttention/get_all_needs_attention_counts.sql")
-    val params = MapSqlParameterSource().addValue("userId", userId)
-    return jdbcTemplate.query(countSql, params, needsAttentionCountRowMapper)
-  }
-
-  override fun getAllNeedsAttentionOldest(userId: Long): List<NeedsAttentionOldest> {
-    val oldestSql = sqlLoader.loadSql("needsAttention/get_all_needs_attention_oldest.sql")
-    val params = MapSqlParameterSource().addValue("userId", userId)
-    return jdbcTemplate.query(oldestSql, params, needsAttentionOldestRowMapper)
-  }
 
   override fun searchForTransactions(
     request: SearchTransactionsRequest,
