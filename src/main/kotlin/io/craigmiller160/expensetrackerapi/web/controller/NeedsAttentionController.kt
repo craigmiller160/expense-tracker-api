e546b159143e1efe6ede90d2b30d9d2c96ffc160
Craig Miller <craigmiller160@gmail.com>
2022-11-14 11:37:36 -0500

    Beginning to migrate needs attention logic to its own controller

---
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/controller/NeedsAttentionController.kt
@@ -0,0 +1,24 @@
+package io.craigmiller160.expensetrackerapi.web.controller
+
+import io.craigmiller160.expensetrackerapi.function.TryEither
+import io.craigmiller160.expensetrackerapi.web.types.NeedsAttentionResponse
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/needs-attention")
+class NeedsAttentionController {
+  @ApiResponse(
+    content =
+      [
+        Content(
+          mediaType = "application/json",
+          schema = Schema(implementation = NeedsAttentionResponse::class))])
+  @GetMapping("/needs-attention")
+  fun getNeedsAttention(): TryEither<NeedsAttentionResponse> =
+    transactionService.getNeedsAttention()
+}
