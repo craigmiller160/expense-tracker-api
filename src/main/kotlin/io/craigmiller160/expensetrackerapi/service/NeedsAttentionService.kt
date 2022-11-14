33ce2ec72c3901b32bcddbe9e2e48a23eb2c9aa3
Craig Miller <craigmiller160@gmail.com>
2022-11-14 11:40:32 -0500

    Implemented service method

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/service/NeedsAttentionService.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/service/NeedsAttentionService.kt
@@ -1,11 +1,41 @@
 package io.craigmiller160.expensetrackerapi.service
 
+import arrow.core.Either
+import io.craigmiller160.expensetrackerapi.data.projection.NeedsAttentionType
 import io.craigmiller160.expensetrackerapi.function.TryEither
+import io.craigmiller160.expensetrackerapi.web.types.CountAndOldest
 import io.craigmiller160.expensetrackerapi.web.types.NeedsAttentionResponse
 import javax.transaction.Transactional
 import org.springframework.stereotype.Service
 
 @Service
 class NeedsAttentionService {
-  @Transactional fun getNeedsAttention(): TryEither<NeedsAttentionResponse> = TODO()
+  @org.springframework.transaction.annotation.Transactional
+  fun getNeedsAttention(): TryEither<NeedsAttentionResponse> {
+    val userId = oAuth2Service.getAuthenticatedUser().userId
+    return Either.catch {
+      val needsAttentionCounts =
+        transactionRepository.getAllNeedsAttentionCounts(userId).associateBy { it.type }
+      val needsAttentionOldest =
+        transactionRepository.getAllNeedsAttentionOldest(userId).associateBy { it.type }
+
+      NeedsAttentionResponse(
+        unconfirmed =
+          CountAndOldest(
+            count = needsAttentionCounts[NeedsAttentionType.UNCONFIRMED]!!.count,
+            oldest = needsAttentionOldest[NeedsAttentionType.UNCONFIRMED]?.date),
+        uncategorized =
+          CountAndOldest(
+            count = needsAttentionCounts[NeedsAttentionType.UNCATEGORIZED]!!.count,
+            oldest = needsAttentionOldest[NeedsAttentionType.UNCATEGORIZED]?.date),
+        duplicate =
+          CountAndOldest(
+            count = needsAttentionCounts[NeedsAttentionType.DUPLICATE]!!.count,
+            oldest = needsAttentionOldest[NeedsAttentionType.DUPLICATE]?.date),
+        possibleRefund =
+          CountAndOldest(
+            count = needsAttentionCounts[NeedsAttentionType.POSSIBLE_REFUND]!!.count,
+            oldest = needsAttentionOldest[NeedsAttentionType.POSSIBLE_REFUND]?.date))
+    }
+  }
 }
--- src/main/kotlin/io/craigmiller160/expensetrackerapi/service/TransactionService.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/service/TransactionService.kt
@@ -12,7 +12,6 @@ import io.craigmiller160.expensetrackerapi.data.model.Category
 import io.craigmiller160.expensetrackerapi.data.model.Transaction
 import io.craigmiller160.expensetrackerapi.data.model.toColumnName
 import io.craigmiller160.expensetrackerapi.data.model.toSpringSortDirection
-import io.craigmiller160.expensetrackerapi.data.projection.NeedsAttentionType
 import io.craigmiller160.expensetrackerapi.data.repository.CategoryRepository
 import io.craigmiller160.expensetrackerapi.data.repository.TransactionRepository
 import io.craigmiller160.expensetrackerapi.data.repository.TransactionViewRepository
@@ -68,35 +67,6 @@ class TransactionService(
   fun deleteTransactions(request: DeleteTransactionsRequest): TryEither<Unit> {
     val userId = oAuth2Service.getAuthenticatedUser().userId
     return Either.catch { transactionRepository.deleteTransactions(request.ids, userId) }
-  }
-
-  @Transactional
-  fun getNeedsAttention(): TryEither<NeedsAttentionResponse> {
-    val userId = oAuth2Service.getAuthenticatedUser().userId
-    return Either.catch {
-      val needsAttentionCounts =
-        transactionRepository.getAllNeedsAttentionCounts(userId).associateBy { it.type }
-      val needsAttentionOldest =
-        transactionRepository.getAllNeedsAttentionOldest(userId).associateBy { it.type }
-
-      NeedsAttentionResponse(
-        unconfirmed =
-          CountAndOldest(
-            count = needsAttentionCounts[NeedsAttentionType.UNCONFIRMED]!!.count,
-            oldest = needsAttentionOldest[NeedsAttentionType.UNCONFIRMED]?.date),
-        uncategorized =
-          CountAndOldest(
-            count = needsAttentionCounts[NeedsAttentionType.UNCATEGORIZED]!!.count,
-            oldest = needsAttentionOldest[NeedsAttentionType.UNCATEGORIZED]?.date),
-        duplicate =
-          CountAndOldest(
-            count = needsAttentionCounts[NeedsAttentionType.DUPLICATE]!!.count,
-            oldest = needsAttentionOldest[NeedsAttentionType.DUPLICATE]?.date),
-        possibleRefund =
-          CountAndOldest(
-            count = needsAttentionCounts[NeedsAttentionType.POSSIBLE_REFUND]!!.count,
-            oldest = needsAttentionOldest[NeedsAttentionType.POSSIBLE_REFUND]?.date))
-    }
   }
 
   @Transactional
