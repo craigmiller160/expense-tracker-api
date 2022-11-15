20c8e8f990536476437eba4b5422b7b958d26647
Craig Miller <craigmiller160@gmail.com>
2022-11-15 17:33:31 -0500

    Implemented rule creation

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/service/AutoCategorizeRuleService.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/service/AutoCategorizeRuleService.kt
@@ -1,7 +1,9 @@
 package io.craigmiller160.expensetrackerapi.service
 
+import arrow.core.Either
 import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.AutoCategorizeRuleId
+import io.craigmiller160.expensetrackerapi.data.model.AutoCategorizeRule
 import io.craigmiller160.expensetrackerapi.data.repository.AutoCategorizeRuleRepository
 import io.craigmiller160.expensetrackerapi.function.TryEither
 import io.craigmiller160.expensetrackerapi.web.types.rules.AutoCategorizeRulePageRequest
@@ -22,7 +24,22 @@ class AutoCategorizeRuleService(
   ): TryEither<AutoCategorizeRulePageResponse> = TODO()
 
   @Transactional
-  fun createRule(request: AutoCategorizeRuleRequest): TryEither<AutoCategorizeRuleResponse> = TODO()
+  fun createRule(request: AutoCategorizeRuleRequest): TryEither<AutoCategorizeRuleResponse> {
+    val userId = oAuth2Service.getAuthenticatedUser().userId
+    val count = autoCategorizeRuleRepository.countAllByUserId(userId)
+    val rule =
+      AutoCategorizeRule(
+        userId = userId,
+        categoryId = request.categoryId,
+        ordinal = (count + 1).toInt(),
+        regex = request.regex,
+        startDate = request.startDate,
+        endDate = request.endDate,
+        minAmount = request.minAmount,
+        maxAmount = request.maxAmount)
+    return Either.catch { autoCategorizeRuleRepository.save(rule) }
+      .map { AutoCategorizeRuleResponse.from(it) }
+  }
 
   @Transactional
   fun updateRule(
