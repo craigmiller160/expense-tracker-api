4719162b7f9ce803772ffa254b4e0281db2c08e3
Craig Miller <craigmiller160@gmail.com>
2022-11-15 16:41:33 -0500

    Stubbed out more endpoints

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/controller/AutoCategorizeRuleController.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/controller/AutoCategorizeRuleController.kt
@@ -1,17 +1,55 @@
 package io.craigmiller160.expensetrackerapi.web.controller
 
+import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
+import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.AutoCategorizeRuleId
 import io.craigmiller160.expensetrackerapi.function.TryEither
 import io.craigmiller160.expensetrackerapi.web.types.rules.AutoCategorizeRulePageRequest
 import io.craigmiller160.expensetrackerapi.web.types.rules.AutoCategorizeRulePageResponse
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import io.craigmiller160.expensetrackerapi.web.types.rules.AutoCategorizeRuleRequest
+import io.craigmiller160.expensetrackerapi.web.types.rules.AutoCategorizeRuleResponse
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/categories/rules")
 class AutoCategorizeRuleController {
+  @ApiResponse(
+    content =
+      [
+        Content(
+          mediaType = "application/json",
+          array =
+            ArraySchema(schema = Schema(implementation = AutoCategorizeRulePageResponse::class)))])
   @GetMapping
   fun getAllRules(
     request: AutoCategorizeRulePageRequest
   ): TryEither<AutoCategorizeRulePageResponse> = TODO()
+
+  @ApiResponse(
+    content =
+      [
+        Content(
+          mediaType = "application/json",
+          array =
+            ArraySchema(schema = Schema(implementation = AutoCategorizeRuleResponse::class)))])
+  @PostMapping
+  fun createRule(
+    @RequestBody request: AutoCategorizeRuleRequest
+  ): TryEither<AutoCategorizeRuleResponse> = TODO()
+
+  @ApiResponse(
+    content =
+      [
+        Content(
+          mediaType = "application/json",
+          array =
+            ArraySchema(schema = Schema(implementation = AutoCategorizeRuleResponse::class)))])
+  @PutMapping("/{ruleId}")
+  fun updateRule(
+    @PathVariable ruleId: TypedId<AutoCategorizeRuleId>,
+    @RequestBody request: AutoCategorizeRuleRequest
+  ): TryEither<AutoCategorizeRuleResponse> = TODO()
 }
