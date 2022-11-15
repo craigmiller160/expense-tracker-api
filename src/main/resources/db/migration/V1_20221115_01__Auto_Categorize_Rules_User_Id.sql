53f71c986ee94a4c687f9fe80a9cf94eec334089
Craig Miller <craigmiller160@gmail.com>
2022-11-15 16:37:01 -0500

    Added some unique constraints

--- src/main/resources/db/migration/V1_20221115_01__Auto_Categorize_Rules_User_Id.sql
+++ src/main/resources/db/migration/V1_20221115_01__Auto_Categorize_Rules_User_Id.sql
@@ -3,3 +3,9 @@ ADD COLUMN user_id BIGINT NOT NULL;
 
 ALTER TABLE last_rule_applied
 ADD COLUMN user_id BIGINT NOT NULL;
+
+ALTER TABLE auto_categorize_rules
+ADD UNIQUE (user_id, ordinal);
+
+ALTER TABLE last_rule_applied
+ADD UNIQUE (user_id, rule_id, transaction_id);
