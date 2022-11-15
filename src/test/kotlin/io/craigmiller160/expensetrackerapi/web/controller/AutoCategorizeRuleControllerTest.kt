c1604ad7000f7a86cfac5c4d9e9494d3e9b8beae
Craig Miller <craigmiller160@gmail.com>
2022-11-15 16:45:04 -0500

    Stubbed out tests for crud apis

---
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/web/controller/AutoCategorizeRuleControllerTest.kt
@@ -0,0 +1,32 @@
+package io.craigmiller160.expensetrackerapi.web.controller
+
+import io.craigmiller160.expensetrackerapi.testcore.ExpenseTrackerIntegrationTest
+import org.junit.jupiter.api.Test
+
+@ExpenseTrackerIntegrationTest
+class AutoCategorizeRuleControllerTest {
+  @Test
+  fun getAllRules() {
+    TODO()
+  }
+
+  @Test
+  fun createRule() {
+    TODO()
+  }
+
+  @Test
+  fun updateRule() {
+    TODO()
+  }
+
+  @Test
+  fun getRule() {
+    TODO()
+  }
+
+  @Test
+  fun deleteRule() {
+    TODO()
+  }
+}
