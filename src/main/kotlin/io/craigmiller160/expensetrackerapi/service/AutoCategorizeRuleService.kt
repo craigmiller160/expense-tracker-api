11b8746ef7c40fb45aa376280d2b5222694ef0e8
Craig Miller <craigmiller160@gmail.com>
2022-11-15 17:12:40 -0500

    Stubbing out more serivce methods

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/service/AutoCategorizeRuleService.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/service/AutoCategorizeRuleService.kt
@@ -1,9 +1,14 @@
 package io.craigmiller160.expensetrackerapi.service
 
+import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
+import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.AutoCategorizeRuleId
 import io.craigmiller160.expensetrackerapi.data.repository.AutoCategorizeRuleRepository
 import io.craigmiller160.expensetrackerapi.function.TryEither
 import io.craigmiller160.expensetrackerapi.web.types.rules.AutoCategorizeRulePageRequest
 import io.craigmiller160.expensetrackerapi.web.types.rules.AutoCategorizeRulePageResponse
+import io.craigmiller160.expensetrackerapi.web.types.rules.AutoCategorizeRuleRequest
+import io.craigmiller160.expensetrackerapi.web.types.rules.AutoCategorizeRuleResponse
+import javax.transaction.Transactional
 import org.springframework.stereotype.Service
 
 @Service
@@ -13,4 +18,13 @@ class AutoCategorizeRuleService(
   fun getAllRules(
     request: AutoCategorizeRulePageRequest
   ): TryEither<AutoCategorizeRulePageResponse> = TODO()
+
+  @Transactional
+  fun createRule(request: AutoCategorizeRuleRequest): TryEither<AutoCategorizeRuleResponse> = TODO()
+
+  @Transactional
+  fun updateRule(
+    ruleId: TypedId<AutoCategorizeRuleId>,
+    request: AutoCategorizeRuleRequest
+  ): TryEither<AutoCategorizeRuleResponse> = TODO()
 }
