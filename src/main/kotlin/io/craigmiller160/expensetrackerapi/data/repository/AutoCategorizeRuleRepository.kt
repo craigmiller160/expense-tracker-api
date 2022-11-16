b5efd7a1f742d9fa8051e901a89774779c70cf8d
Craig Miller <craigmiller160@gmail.com>
2022-11-16 11:30:52 -0500

    Added repo method

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/data/repository/AutoCategorizeRuleRepository.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/repository/AutoCategorizeRuleRepository.kt
@@ -4,6 +4,8 @@ import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.AutoCategorizeRuleId
 import io.craigmiller160.expensetrackerapi.data.model.AutoCategorizeRule
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface AutoCategorizeRuleRepository :
   JpaRepository<AutoCategorizeRule, TypedId<AutoCategorizeRuleId>>,
@@ -13,4 +15,13 @@ interface AutoCategorizeRuleRepository :
   fun findByIdAndUserId(id: TypedId<AutoCategorizeRuleId>, userId: Long): AutoCategorizeRule?
 
   fun deleteByIdAndUserId(id: TypedId<AutoCategorizeRuleId>, userId: Long)
+
+  @Query(
+    """
+    UPDATE AutoCategorizeRule r
+    SET r.ordinal = r.ordinal - 1
+    WHERE r.ordinal < :maxOrdinal
+    AND r.ordinal >= :minOrdinal
+  """)
+  fun decrementOrdinals(@Param("minOrdinal") minOrdinal: Int, @Param("maxOrdinal") maxOrdinal: Int)
 }
