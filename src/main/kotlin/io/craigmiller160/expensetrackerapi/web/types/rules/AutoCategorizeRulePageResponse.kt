c8da9361c39a3836238d22b5d3e90ff0ee7ff4c5
Craig Miller <craigmiller160@gmail.com>
2022-11-16 07:58:26 -0500

    Some dto logic

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/rules/AutoCategorizeRulePageResponse.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/rules/AutoCategorizeRulePageResponse.kt
@@ -1,9 +1,19 @@
 package io.craigmiller160.expensetrackerapi.web.types.rules
 
+import io.craigmiller160.expensetrackerapi.data.model.AutoCategorizeRule
 import io.craigmiller160.expensetrackerapi.web.types.PageableResponse
+import org.springframework.data.domain.Page
 
 data class AutoCategorizeRulePageResponse(
   val rules: List<AutoCategorizeRuleResponse>,
   override val pageNumber: Int,
   override val totalItems: Long
-) : PageableResponse
+) : PageableResponse {
+  companion object {
+    fun from(page: Page<AutoCategorizeRule>): AutoCategorizeRulePageResponse =
+      AutoCategorizeRulePageResponse(
+        rules = page.content.map { AutoCategorizeRuleResponse.from(it) },
+        pageNumber = page.number,
+        totalItems = page.totalElements)
+  }
+}
