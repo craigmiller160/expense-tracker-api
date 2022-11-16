29fd951bd67347aac76e09ec282133c35ca79a25
Craig Miller <craigmiller160@gmail.com>
2022-11-16 07:40:37 -0500

    Tweaked request parameters

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/rules/AutoCategorizeRulePageRequest.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/rules/AutoCategorizeRulePageRequest.kt
@@ -1,6 +1,11 @@
 package io.craigmiller160.expensetrackerapi.web.types.rules
 
+import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
+import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.CategoryId
 import io.craigmiller160.expensetrackerapi.web.types.PageableRequest
 
-data class AutoCategorizeRulePageRequest(override val pageNumber: Int, override val pageSize: Int) :
-  PageableRequest
+data class AutoCategorizeRulePageRequest(
+  override val pageNumber: Int,
+  override val pageSize: Int,
+  val categoryId: TypedId<CategoryId>? = null
+) : PageableRequest
