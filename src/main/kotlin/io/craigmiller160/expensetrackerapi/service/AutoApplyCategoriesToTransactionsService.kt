55b324f866981f615a9bcabb0fc6e1295eb85a8f
Craig Miller <craigmiller160@gmail.com>
2022-11-17 11:14:54 -0500

    Added streaming solution

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/service/AutoApplyCategoriesToTransactionsService.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/service/AutoApplyCategoriesToTransactionsService.kt
@@ -1,11 +1,14 @@
 package io.craigmiller160.expensetrackerapi.service
 
 import io.craigmiller160.expensetrackerapi.data.model.Transaction
+import io.craigmiller160.expensetrackerapi.data.repository.AutoCategorizeRuleRepository
 import io.craigmiller160.expensetrackerapi.function.TryEither
 import org.springframework.stereotype.Service
 
 @Service
-class AutoApplyCategoriesToTransactionsService {
+class AutoApplyCategoriesToTransactionsService(
+  private val autoCategorizeRuleRepository: AutoCategorizeRuleRepository
+) {
   fun applyCategoriesToTransactions(transactions: List<Transaction>): TryEither<List<Transaction>> =
     TODO()
 }
