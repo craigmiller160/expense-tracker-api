34246d4f303db449f957dfb0c3b0353d3534b071
Craig Miller <craigmiller160@gmail.com>
2022-11-17 11:18:56 -0500

    Test setup tweaks

--- src/test/kotlin/io/craigmiller160/expensetrackerapi/service/AutoApplyCategoriesToTransactionsServiceTest.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/service/AutoApplyCategoriesToTransactionsServiceTest.kt
@@ -1,10 +1,18 @@
 package io.craigmiller160.expensetrackerapi.service
 
 import io.craigmiller160.expensetrackerapi.testcore.ExpenseTrackerIntegrationTest
+import io.craigmiller160.expensetrackerapi.testutils.DataHelper
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 
 @ExpenseTrackerIntegrationTest
-class AutoApplyCategoriesToTransactionsServiceTest {
+class AutoApplyCategoriesToTransactionsServiceTest
+@Autowired
+constructor(private val dataHelper: DataHelper) {
+
+  @BeforeEach fun setup() {}
+
   @Test
   fun applyCategoriesToTransactions() {
     TODO()
