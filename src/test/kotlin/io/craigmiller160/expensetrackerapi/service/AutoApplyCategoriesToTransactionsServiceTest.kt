c0f7c7ce02dc9a0e7b28d4ebf4defb3d07eac1ef
Craig Miller <craigmiller160@gmail.com>
2022-11-17 11:18:08 -0500

    Stubbed out test

---
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/service/AutoApplyCategoriesToTransactionsServiceTest.kt
@@ -0,0 +1,12 @@
+package io.craigmiller160.expensetrackerapi.service
+
+import io.craigmiller160.expensetrackerapi.testcore.ExpenseTrackerIntegrationTest
+import org.junit.jupiter.api.Test
+
+@ExpenseTrackerIntegrationTest
+class AutoApplyCategoriesToTransactionsServiceTest {
+  @Test
+  fun applyCategoriesToTransactions() {
+    TODO()
+  }
+}
