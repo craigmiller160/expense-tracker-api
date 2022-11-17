9275d80024c779d828cac624036c074f23ce0866
Craig Miller <craigmiller160@gmail.com>
2022-11-17 11:03:46 -0500

    Refactoring another test

--- src/test/kotlin/io/craigmiller160/expensetrackerapi/web/controller/AutoCategorizeRuleControllerTest.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/web/controller/AutoCategorizeRuleControllerTest.kt
@@ -527,6 +527,8 @@ constructor(
 
   @Test
   fun reOrderRule_invalidRule() {
+    dataHelper.createRule(1L, cat1.id)
+
     val id = TypedId<AutoCategorizeRuleId>()
     mockMvc
       .put("/categories/rules/$id/reOrder/1") {
@@ -535,7 +537,7 @@ constructor(
       }
       .andExpect {
         status { isBadRequest() }
-        jsonPath("$.message", equalTo(""))
+        jsonPath("$.message", equalTo("Invalid Rule: $id"))
       }
   }
 
