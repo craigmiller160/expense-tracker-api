297a5b06a023501e53cedadfedc55292330a5177
Craig Miller <craigmiller160@gmail.com>
2022-11-21 15:21:38 -0500

    More setup for custom repo logic

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/data/repository/impl/LastRuleAppliedRepositoryCustomImpl.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/repository/impl/LastRuleAppliedRepositoryCustomImpl.kt
@@ -2,12 +2,17 @@ package io.craigmiller160.expensetrackerapi.data.repository.impl
 
 import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.TransactionId
+import io.craigmiller160.expensetrackerapi.data.SqlLoader
 import io.craigmiller160.expensetrackerapi.data.projection.LastRuleAppliedForTransaction
 import io.craigmiller160.expensetrackerapi.data.repository.LastRuleAppliedRepositoryCustom
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Repository
 
 @Repository
-class LastRuleAppliedRepositoryCustomImpl : LastRuleAppliedRepositoryCustom {
+class LastRuleAppliedRepositoryCustomImpl(
+  private val jdbcTemplate: NamedParameterJdbcTemplate,
+  private val sqlLoader: SqlLoader
+) : LastRuleAppliedRepositoryCustom {
   override fun getLastRuleDetailsForTransaction(
     userId: Long,
     transactionId: TypedId<TransactionId>
