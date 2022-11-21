53b28eacd7a8ab35ad3a5a182049c95bdfdc7538
Craig Miller <craigmiller160@gmail.com>
2022-11-21 14:41:48 -0500

    Finished API signature

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/controller/LastRuleAppliedController.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/controller/LastRuleAppliedController.kt
@@ -1,14 +1,31 @@
 package io.craigmiller160.expensetrackerapi.web.controller
 
+import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
+import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.TransactionId
+import io.craigmiller160.expensetrackerapi.function.TryEither
+import io.craigmiller160.expensetrackerapi.web.types.rules.LastRuleAppliedResponse
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/transactions/rules/last-applied")
 class LastRuleAppliedController {
-  @GetMapping
-  fun getLastAppliedRuleForTransaction() {
+
+  @ApiResponse(
+    content =
+      [
+        Content(
+          mediaType = "application/json",
+          schema = Schema(implementation = LastRuleAppliedResponse::class))])
+  @GetMapping("/{transactionId}")
+  fun getLastAppliedRuleForTransaction(
+    @PathVariable transactionId: TypedId<TransactionId>
+  ): TryEither<LastRuleAppliedResponse> {
     TODO()
   }
 }
