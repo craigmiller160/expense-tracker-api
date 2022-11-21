7d33fc9ad4decf007f47841551de63c72561f1f6
Craig Miller <craigmiller160@gmail.com>
2022-11-21 14:42:45 -0500

    Stubbed out tests

---
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/web/controller/LastRuleAppliedControllerTest.kt
@@ -0,0 +1,17 @@
+package io.craigmiller160.expensetrackerapi.web.controller
+
+import io.craigmiller160.expensetrackerapi.testcore.ExpenseTrackerIntegrationTest
+import org.junit.jupiter.api.Test
+
+@ExpenseTrackerIntegrationTest
+class LastRuleAppliedControllerTest {
+  @Test
+  fun `getLastAppliedRuleForTransaction - last applied exists`() {
+    TODO()
+  }
+
+  @Test
+  fun `getLastAppliedRuleForTransaction - last applied does not exist`() {
+    TODO()
+  }
+}
