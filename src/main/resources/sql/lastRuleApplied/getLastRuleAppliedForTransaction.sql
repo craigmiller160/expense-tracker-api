3a9528258ace9d1c60ab3175294c38b5a930878e
Craig Miller <craigmiller160@gmail.com>
2022-11-21 15:18:45 -0500

    Added new sql file

---
+++ src/main/resources/sql/lastRuleApplied/getLastRuleAppliedForTransaction.sql
@@ -0,0 +1,6 @@
+SELECT last_rule.transaction_id, last_rule.rule_id, last_rule.user_id,
+    rule.ordinal, rule.category_id, rule.regex, rule.start_date, rule.end_date,
+    rule.min_amount, rule.max_amount
+FROM last_rule_applied last_rule
+JOIN auto_categorize_rules rule ON last_rule.rule_id = rule.id
+WHERE last_rule.transaction_id = :transactionId
