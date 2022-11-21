5838140fc42f9ed0b623ba3b263314b62f174ce7
Craig Miller <craigmiller160@gmail.com>
2022-11-21 15:40:58 -0500

    Swagger improvements

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ The backend for a program to help me track my expenses.
 Create a Postgres database `expense_tracker_dev`, if it isn't already.
 
 Then use the command `mvnd spring-boot:run`.
+
+## Swagger
+
+The swagger can be accessed locally via `https://localhost:8080/swagger-ui/index.html`
--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/controller/LastRuleAppliedController.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/controller/LastRuleAppliedController.kt
@@ -8,6 +8,7 @@ import io.craigmiller160.expensetrackerapi.web.types.rules.LastRuleAppliedRespon
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -18,12 +19,17 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/transactions/rules/last-applied")
 class LastRuleAppliedController(private val lastRuleAppliedService: LastRuleAppliedService) {
 
-  @ApiResponse(
-    content =
+  @ApiResponses(
+    value =
       [
-        Content(
-          mediaType = "application/json",
-          schema = Schema(implementation = LastRuleAppliedResponse::class))])
+        ApiResponse(
+          responseCode = "200",
+          content =
+            [
+              Content(
+                mediaType = "application/json",
+                schema = Schema(implementation = LastRuleAppliedResponse::class))]),
+        ApiResponse(responseCode = "204", content = [Content(schema = Schema(hidden = true))])])
   @GetMapping("/{transactionId}")
   fun getLastAppliedRuleForTransaction(
     @PathVariable transactionId: TypedId<TransactionId>
