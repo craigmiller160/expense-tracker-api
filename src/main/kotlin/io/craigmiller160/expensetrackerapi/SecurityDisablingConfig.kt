5aeb301c8015a64632c9d8c2cf59365861d53f74
Craig Miller <craigmiller160@gmail.com>
2022-11-22 14:51:10 -0500

    Cleanup

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/SecurityDisablingConfig.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/SecurityDisablingConfig.kt
@@ -1,24 +1,21 @@
 package io.craigmiller160.expensetrackerapi
 
-import io.craigmiller160.oauth2.dto.AuthUserDto
-import io.craigmiller160.oauth2.service.OAuth2Service
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 // TODO delete this
 @Configuration
 class SecurityDisablingConfig {
-  @Bean
-  fun oAuth2Service(): OAuth2Service =
-    object : OAuth2Service {
-      override fun getAuthenticatedUser(): AuthUserDto =
-        AuthUserDto(
-          userId = 1L,
-          username = "craigmiller160@gmail.com",
-          firstName = "Craig",
-          lastName = "Miller",
-          roles = listOf())
-
-      override fun logout(): String = ""
-    }
+  //  @Bean
+  //  fun oAuth2Service(): OAuth2Service =
+  //    object : OAuth2Service {
+  //      override fun getAuthenticatedUser(): AuthUserDto =
+  //        AuthUserDto(
+  //          userId = 1L,
+  //          username = "craigmiller160@gmail.com",
+  //          firstName = "Craig",
+  //          lastName = "Miller",
+  //          roles = listOf())
+  //
+  //      override fun logout(): String = ""
+  //    }
 }
