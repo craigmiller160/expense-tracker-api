729da7fe0757f57f49e90d95fac18acd06081b1e
Craig Miller <craigmiller160@gmail.com>
2022-11-22 14:34:01 -0500

    Restored oauth2 utils config

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/config/OAuth2UtilsConfig.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/config/OAuth2UtilsConfig.kt
@@ -1,6 +1,8 @@
 package io.craigmiller160.expensetrackerapi.config
 
-// TODO restore this
-// @Configuration
-// @ComponentScan(basePackages = ["io.craigmiller160.spring.oauth2", "io.craigmiller160.oauth2"])
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ComponentScan(basePackages = ["io.craigmiller160.spring.oauth2", "io.craigmiller160.oauth2"])
 class OAuth2UtilsConfig
