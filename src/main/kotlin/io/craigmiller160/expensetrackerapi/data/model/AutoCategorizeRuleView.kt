ecf5d792e37935d4b3d03862d459618fe3ba439a
Craig Miller <craigmiller160@gmail.com>
2022-11-22 11:09:26 -0500

    Added entity annotations

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/AutoCategorizeRuleView.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/AutoCategorizeRuleView.kt
@@ -3,11 +3,19 @@ package io.craigmiller160.expensetrackerapi.data.model
 import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.AutoCategorizeRuleId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.CategoryId
+import io.craigmiller160.expensetrackerapi.common.data.typedid.jpatype.TypedIdJpaType
 import java.math.BigDecimal
 import java.time.LocalDate
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+import org.hibernate.annotations.TypeDef
 
+@Entity
+@Table(name = "auto_categorize_rules_view")
+@TypeDef(defaultForType = TypedId::class, typeClass = TypedIdJpaType::class)
 data class AutoCategorizeRuleView(
-  val id: TypedId<AutoCategorizeRuleId>,
+  @Id val id: TypedId<AutoCategorizeRuleId>,
   val categoryId: TypedId<CategoryId>,
   val categoryName: String,
   val userId: Long,
