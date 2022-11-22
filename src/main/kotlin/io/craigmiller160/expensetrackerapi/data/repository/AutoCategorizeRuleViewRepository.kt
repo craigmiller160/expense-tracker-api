f135c0e945ef9511c8d02fe5923e928ff04a2282
Craig Miller <craigmiller160@gmail.com>
2022-11-22 11:09:58 -0500

    Added repo

---
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/repository/AutoCategorizeRuleViewRepository.kt
@@ -0,0 +1,9 @@
+package io.craigmiller160.expensetrackerapi.data.repository
+
+import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
+import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.AutoCategorizeRuleId
+import io.craigmiller160.expensetrackerapi.data.model.AutoCategorizeRuleView
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface AutoCategorizeRuleViewRepository :
+  JpaRepository<AutoCategorizeRuleView, TypedId<AutoCategorizeRuleId>>
