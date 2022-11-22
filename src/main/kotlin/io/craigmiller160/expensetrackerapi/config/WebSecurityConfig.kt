168b5e59d7600e1c76225917b017a93903be0572
Craig Miller <craigmiller160@gmail.com>
2022-11-22 10:37:13 -0500

    More disabling of security

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/config/WebSecurityConfig.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/config/WebSecurityConfig.kt
@@ -1,6 +1,5 @@
 package io.craigmiller160.expensetrackerapi.config
 
-import io.craigmiller160.spring.oauth2.security.JwtValidationFilterConfigurer
 import io.craigmiller160.webutils.security.AuthEntryPoint
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -12,7 +11,7 @@ import org.springframework.security.web.SecurityFilterChain
 @Configuration
 @EnableWebSecurity
 class WebSecurityConfig(
-  private val jwtFilterConfigurer: JwtValidationFilterConfigurer,
+  //  private val jwtFilterConfigurer: JwtValidationFilterConfigurer,
   private val authEntryPoint: AuthEntryPoint
 ) {
   @Bean
