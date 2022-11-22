8dc1bb1e50ee166ab63204c18accb10a51bbd332
Craig Miller <craigmiller160@gmail.com>
2022-11-22 11:07:44 -0500

    Added view

---
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/AutoCategorizeRuleView.kt
@@ -0,0 +1,17 @@
+package io.craigmiller160.expensetrackerapi.data.model
+
+import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
+import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.AutoCategorizeRuleId
+import java.math.BigDecimal
+import java.time.LocalDate
+
+data class AutoCategorizeRuleView(
+  val id: TypedId<AutoCategorizeRuleId>,
+  val userId: Long,
+  val ordinal: Int,
+  val regex: String,
+  val startDate: LocalDate? = null,
+  val endDate: LocalDate? = null,
+  val minAmount: BigDecimal? = null,
+  val maxAmount: BigDecimal? = null
+)
