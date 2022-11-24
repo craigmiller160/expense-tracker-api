94aface04227ffceef717c22be7eb7522f31a327
Craig Miller <craigmiller160@gmail.com>
2022-11-24 10:51:50 -0500

    Refactored test entities

--- src/test/kotlin/io/craigmiller160/expensetrackerapi/data/model/Country.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/data/model/Country.kt
@@ -1,19 +1,11 @@
 package io.craigmiller160.expensetrackerapi.data.model
 
-import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
-import io.craigmiller160.expensetrackerapi.common.data.typedid.jpatype.TypedIdJpaType
-import io.craigmiller160.expensetrackerapi.data.model.core.ImmutableLegacyEntity
-import java.time.ZonedDateTime
+import io.craigmiller160.expensetrackerapi.data.model.core.ImmutableEntity
 import javax.persistence.Entity
-import javax.persistence.Id
 import javax.persistence.Table
-import org.hibernate.annotations.TypeDef
 
 @Entity
 @Table(name = "countries")
-@TypeDef(defaultForType = TypedId::class, typeClass = TypedIdJpaType::class)
-data class Country(
-  val name: String,
-  @Id override val id: TypedId<CountryId> = TypedId(),
-  override val created: ZonedDateTime = ZonedDateTime.now()
-) : ImmutableLegacyEntity<CountryId>
+class Country : ImmutableEntity<CountryId>() {
+  var name: String = ""
+}
--- src/test/kotlin/io/craigmiller160/expensetrackerapi/data/model/Resident.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/data/model/Resident.kt
@@ -1,22 +1,11 @@
 package io.craigmiller160.expensetrackerapi.data.model
 
-import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
-import io.craigmiller160.expensetrackerapi.common.data.typedid.jpatype.TypedIdJpaType
-import io.craigmiller160.expensetrackerapi.data.model.core.MutableLegacyEntity
-import java.time.ZonedDateTime
+import io.craigmiller160.expensetrackerapi.data.model.core.MutableEntity
 import javax.persistence.Entity
-import javax.persistence.Id
 import javax.persistence.Table
-import javax.persistence.Version
-import org.hibernate.annotations.TypeDef
 
 @Entity
 @Table(name = "residents")
-@TypeDef(defaultForType = TypedId::class, typeClass = TypedIdJpaType::class)
-data class Resident(
-  val name: String,
-  @Id override val id: TypedId<ResidentId> = TypedId(),
-  override val created: ZonedDateTime = ZonedDateTime.now(),
-  override var updated: ZonedDateTime = ZonedDateTime.now(),
-  @Version override val version: Long = 1
-) : MutableLegacyEntity<ResidentId>
+class Resident : MutableEntity<ResidentId>() {
+  var name: String = ""
+}
