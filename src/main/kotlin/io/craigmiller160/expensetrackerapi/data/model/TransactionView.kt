a73f34cbf5e2d5711110a53af4101211944f6ebe
Craig Miller <craigmiller160@gmail.com>
2022-11-24 14:41:42 -0500

    Refactored another view

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/TransactionView.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/TransactionView.kt
@@ -3,31 +3,25 @@ package io.craigmiller160.expensetrackerapi.data.model
 import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.CategoryId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.TransactionId
-import io.craigmiller160.expensetrackerapi.common.data.typedid.jpatype.TypedIdJpaType
+import io.craigmiller160.expensetrackerapi.data.model.core.ViewEntity
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.ZonedDateTime
 import javax.persistence.Entity
-import javax.persistence.Id
 import javax.persistence.Table
-import org.hibernate.annotations.TypeDef
 
-// TODO does this need a parent?
-// TODO refactor
 @Entity
 @Table(name = "transactions_view")
-@TypeDef(defaultForType = TypedId::class, typeClass = TypedIdJpaType::class)
-data class TransactionView(
-  @Id override var id: TypedId<TransactionId>,
+class TransactionView(
   override var userId: Long,
   override var expenseDate: LocalDate,
   override var description: String,
   override var amount: BigDecimal,
-  val contentHash: String,
+  var contentHash: String,
   override var categoryId: TypedId<CategoryId>?,
-  val categoryName: String?,
+  var categoryName: String?,
   override var confirmed: Boolean,
-  val duplicate: Boolean,
-  val created: ZonedDateTime,
-  val updated: ZonedDateTime
-) : TransactionCommon
+  var duplicate: Boolean,
+  var created: ZonedDateTime,
+  var updated: ZonedDateTime
+) : ViewEntity<TransactionId>(), TransactionCommon
