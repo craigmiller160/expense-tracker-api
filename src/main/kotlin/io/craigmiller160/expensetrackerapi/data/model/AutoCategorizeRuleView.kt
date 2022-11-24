49a0ba9ff385ee5f0e06a1c8b764ad5a8679ba32
Craig Miller <craigmiller160@gmail.com>
2022-11-24 14:40:56 -0500

    Refactored a view

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/AutoCategorizeRuleView.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/AutoCategorizeRuleView.kt
@@ -3,28 +3,22 @@ package io.craigmiller160.expensetrackerapi.data.model
 import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.AutoCategorizeRuleId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.CategoryId
-import io.craigmiller160.expensetrackerapi.common.data.typedid.jpatype.TypedIdJpaType
+import io.craigmiller160.expensetrackerapi.data.model.core.ViewEntity
 import java.math.BigDecimal
 import java.time.LocalDate
 import javax.persistence.Entity
-import javax.persistence.Id
 import javax.persistence.Table
-import org.hibernate.annotations.TypeDef
 
-// TODO does this need a parent?
-// TODO refactor
 @Entity
 @Table(name = "auto_categorize_rules_view")
-@TypeDef(defaultForType = TypedId::class, typeClass = TypedIdJpaType::class)
-data class AutoCategorizeRuleView(
-  @Id val id: TypedId<AutoCategorizeRuleId>,
-  val categoryId: TypedId<CategoryId>,
-  val categoryName: String,
-  val userId: Long,
-  val ordinal: Int,
-  val regex: String,
-  val startDate: LocalDate? = null,
-  val endDate: LocalDate? = null,
-  val minAmount: BigDecimal? = null,
-  val maxAmount: BigDecimal? = null
-)
+class AutoCategorizeRuleView(
+  var categoryId: TypedId<CategoryId>,
+  var categoryName: String,
+  var userId: Long,
+  var ordinal: Int,
+  var regex: String,
+  var startDate: LocalDate? = null,
+  var endDate: LocalDate? = null,
+  var minAmount: BigDecimal? = null,
+  var maxAmount: BigDecimal? = null
+) : ViewEntity<AutoCategorizeRuleId>()
