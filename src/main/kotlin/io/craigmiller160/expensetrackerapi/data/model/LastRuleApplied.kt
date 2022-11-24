3c5ce94cbd846112d841a028c200dfbba42a038a
Craig Miller <craigmiller160@gmail.com>
2022-11-24 14:15:13 -0500

    More refactoring

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/LastRuleApplied.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/LastRuleApplied.kt
@@ -4,22 +4,14 @@ import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.AutoCategorizeRuleId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.LastRuleAppliedId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.TransactionId
-import io.craigmiller160.expensetrackerapi.common.data.typedid.jpatype.TypedIdJpaType
-import io.craigmiller160.expensetrackerapi.data.model.core.ImmutableLegacyEntity
-import java.time.ZonedDateTime
+import io.craigmiller160.expensetrackerapi.data.model.core.ImmutableEntity
 import javax.persistence.Entity
-import javax.persistence.Id
 import javax.persistence.Table
-import org.hibernate.annotations.TypeDef
 
-// TODO refactor
 @Entity
 @Table(name = "last_rule_applied")
-@TypeDef(defaultForType = TypedId::class, typeClass = TypedIdJpaType::class)
-data class LastRuleApplied(
-  val userId: Long,
-  val ruleId: TypedId<AutoCategorizeRuleId>,
-  val transactionId: TypedId<TransactionId>,
-  @Id override val id: TypedId<LastRuleAppliedId> = TypedId(),
-  override val created: ZonedDateTime = ZonedDateTime.now()
-) : ImmutableLegacyEntity<LastRuleAppliedId>
+class LastRuleApplied(
+  var userId: Long,
+  var ruleId: TypedId<AutoCategorizeRuleId>,
+  var transactionId: TypedId<TransactionId>
+) : ImmutableEntity<LastRuleAppliedId>()
