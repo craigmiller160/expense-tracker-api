ecbd79e19d47277cbc9008cd8de86b12680893e8
Craig Miller <craigmiller160@gmail.com>
2022-11-24 14:29:49 -0500

    Added copy constructor to transaction

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/Transaction.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/Transaction.kt
@@ -22,4 +22,16 @@ class Transaction(
   var markNotDuplicateNano: Long? = null,
   override var confirmed: Boolean = false,
   override var categoryId: TypedId<CategoryId>? = null,
-) : MutableEntity<TransactionId>(), TransactionCommon
+) : MutableEntity<TransactionId>(), TransactionCommon {
+  constructor(
+    other: Transaction
+  ) : this(
+    userId = other.userId,
+    expenseDate = other.expenseDate,
+    description = other.description,
+    amount = other.amount,
+    contentHash = other.contentHash,
+    markNotDuplicateNano = other.markNotDuplicateNano,
+    confirmed = other.confirmed,
+    categoryId = other.categoryId)
+}
