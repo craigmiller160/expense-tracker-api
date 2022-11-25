66405a4207296aa30a523d0b8208a9949f657cf6
Craig Miller <craigmiller160@gmail.com>
2022-11-24 22:44:29 -0500

    Restored persistable

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/core/DatabaseRecord.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/core/DatabaseRecord.kt
@@ -2,16 +2,26 @@ package io.craigmiller160.expensetrackerapi.data.model.core
 
 import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.jpatype.TypedIdJpaType
-import javax.persistence.Column
-import javax.persistence.Id
-import javax.persistence.MappedSuperclass
+import javax.persistence.*
+import kotlin.jvm.Transient
 import org.hibernate.annotations.TypeDef
+import org.springframework.data.domain.Persistable
 
 @MappedSuperclass
 @TypeDef(defaultForType = TypedId::class, typeClass = TypedIdJpaType::class)
-abstract class DatabaseRecord<T> {
+abstract class DatabaseRecord<T> : Persistable<TypedId<T>> {
   // TODO rename this, then rename the DB columns to match
   @Id @Column(name = "id") var recordId: TypedId<T> = TypedId()
+  @Transient private var innerIsNew: Boolean = true
+  override fun getId(): TypedId<T> = recordId
+
+  override fun isNew(): Boolean = innerIsNew
+
+  @PrePersist
+  @PostLoad
+  fun handleIsNew() {
+    innerIsNew = false
+  }
 
   override fun equals(other: Any?): Boolean {
     if (other !is TableEntity<*>) return false
