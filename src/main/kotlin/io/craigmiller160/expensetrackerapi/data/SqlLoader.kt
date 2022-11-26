bedaba7959fab912b6ae1d11639af69d8863755f
Craig Miller <craigmiller160@gmail.com>
2022-11-26 13:04:16 -0500

    Integrated template into sql loader

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/data/SqlLoader.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/SqlLoader.kt
@@ -1,7 +1,7 @@
 package io.craigmiller160.expensetrackerapi.data
 
 import com.github.mustachejava.DefaultMustacheFactory
-import com.github.mustachejava.Mustache
+import io.craigmiller160.expensetrackerapi.data.mustache.MustacheSqlTemplate
 import java.util.concurrent.ConcurrentHashMap
 import org.springframework.core.io.ResourceLoader
 import org.springframework.stereotype.Component
@@ -9,13 +9,15 @@ import org.springframework.stereotype.Component
 @Component
 class SqlLoader(private val resourceLoader: ResourceLoader) {
   private val rawSqlCache = ConcurrentHashMap<String, String>()
-  private val sqlMustacheCache = ConcurrentHashMap<String, Mustache>()
+  private val sqlMustacheCache = ConcurrentHashMap<String, MustacheSqlTemplate>()
   private val mustacheFactory = DefaultMustacheFactory()
   fun loadSql(fileName: String): String =
     rawSqlCache.computeIfAbsent(fileName) {
       resourceLoader.getResource("classpath:sql/$it").inputStream.reader().readText()
     }
 
-  fun loadSqlMustacheTemplate(fileName: String): Mustache =
-    sqlMustacheCache.computeIfAbsent(fileName) { loadSql(fileName).let(mustacheFactory::compile) }
+  fun loadSqlMustacheTemplate(fileName: String): MustacheSqlTemplate =
+    sqlMustacheCache.computeIfAbsent(fileName) {
+      loadSql(fileName).let(mustacheFactory::compile).let { MustacheSqlTemplate(it) }
+    }
 }
