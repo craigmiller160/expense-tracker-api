95070f0f3face3f5eada9efaec914d9dd5d96582
Craig Miller <craigmiller160@gmail.com>
2022-11-26 12:49:40 -0500

    Integrated mustache into sqlloader

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/data/SqlLoader.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/SqlLoader.kt
@@ -1,14 +1,21 @@
 package io.craigmiller160.expensetrackerapi.data
 
+import com.github.mustachejava.DefaultMustacheFactory
+import com.github.mustachejava.Mustache
 import java.util.concurrent.ConcurrentHashMap
 import org.springframework.core.io.ResourceLoader
 import org.springframework.stereotype.Component
 
 @Component
 class SqlLoader(private val resourceLoader: ResourceLoader) {
-  private val cache = ConcurrentHashMap<String, String>()
+  private val rawSqlCache = ConcurrentHashMap<String, String>()
+  private val sqlMustacheCache = ConcurrentHashMap<String, Mustache>()
+  private val mustacheFactory = DefaultMustacheFactory()
   fun loadSql(fileName: String): String =
-    cache.computeIfAbsent(fileName) {
+    rawSqlCache.computeIfAbsent(fileName) {
       resourceLoader.getResource("classpath:sql/$it").inputStream.reader().readText()
     }
+
+  fun loadSqlMustacheTemplate(fileName: String): Mustache =
+    sqlMustacheCache.computeIfAbsent(fileName) { loadSql(fileName).let(mustacheFactory::compile) }
 }
