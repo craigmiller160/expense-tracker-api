1e211f979c8a8395677e406224659444429ab5fd
Craig Miller <craigmiller160@gmail.com>
2022-11-28 10:45:38 -0500

    Cleanup

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/service/parsing/CsvParser.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/service/parsing/CsvParser.kt
@@ -7,8 +7,6 @@ import java.io.InputStream
 import java.io.InputStreamReader
 
 object CsvParser {
-  fun parse2(stream: InputStream): TryEither<Sequence<Array<String>>> =
-    Either.catch { CSVReader(InputStreamReader(stream)).readAll().asSequence().drop(1) }
 
   fun parse(stream: InputStream): TryEither<CsvData> =
     Either.catch { CSVReader(InputStreamReader(stream)).readAll() }
