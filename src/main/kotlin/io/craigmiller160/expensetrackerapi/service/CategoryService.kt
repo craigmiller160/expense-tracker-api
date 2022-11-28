c4087ef83553afd95baa7c77d5ea004f11b9aba9
Craig Miller <craigmiller160@gmail.com>
2022-11-28 11:19:10 -0500

    Integrated validation into request and update category flows

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/service/CategoryService.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/service/CategoryService.kt
@@ -3,6 +3,8 @@ package io.craigmiller160.expensetrackerapi.service
 import arrow.core.Either
 import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.CategoryId
+import io.craigmiller160.expensetrackerapi.common.error.BadRequestException
+import io.craigmiller160.expensetrackerapi.data.constants.CategoryConstants
 import io.craigmiller160.expensetrackerapi.data.model.Category
 import io.craigmiller160.expensetrackerapi.data.repository.CategoryRepository
 import io.craigmiller160.expensetrackerapi.data.repository.TransactionRepository
@@ -28,10 +30,19 @@ class CategoryService(
     }
   }
 
+  private fun validateRequest(request: CategoryRequest): TryEither<CategoryRequest> {
+    if (CategoryConstants.UNKNOWN_CATEGORY_NAME == request.name) {
+      return Either.Left(
+        BadRequestException("Illegal category name: ${CategoryConstants.UNKNOWN_CATEGORY_NAME}"))
+    }
+    return Either.Right(request)
+  }
+
   @Transactional
   fun createCategory(request: CategoryRequest): TryEither<CategoryResponse> {
     val userId = oAuth2Service.getAuthenticatedUser().userId
-    return Either.catch {
+    return validateRequest(request)
+      .flatMapCatch {
         categoryRepository.save(
           Category(name = request.name, userId = userId, color = StringToColor.get(request.name)))
       }
@@ -41,7 +52,8 @@ class CategoryService(
   @Transactional
   fun updateCategory(categoryId: TypedId<CategoryId>, request: CategoryRequest): TryEither<Unit> {
     val userId = oAuth2Service.getAuthenticatedUser().userId
-    return Either.catch { categoryRepository.findByUidAndUserId(categoryId, userId) }
+    return validateRequest(request)
+      .flatMapCatch { categoryRepository.findByUidAndUserId(categoryId, userId) }
       .flatMapCatch { nullableCategory ->
         nullableCategory?.let { category ->
           categoryRepository.save(
