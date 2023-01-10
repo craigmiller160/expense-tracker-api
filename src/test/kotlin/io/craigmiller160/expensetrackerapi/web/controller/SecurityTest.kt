664241dff6cbd3cc3ec674a6fc357e1ee59588a6
Craig Miller <craigmiller160@gmail.com>
2023-01-10 16:56:37 -0500

    Stubbed out new tests

---
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/web/controller/SecurityTest.kt
@@ -0,0 +1,25 @@
+package io.craigmiller160.expensetrackerapi.web.controller
+
+import org.junit.jupiter.api.Test
+
+class SecurityTest {
+  @Test
+  fun `allows valid token with access role`() {
+    TODO()
+  }
+
+  @Test
+  fun `rejects valid token without access role`() {
+    TODO()
+  }
+
+  @Test
+  fun `allows healthcheck without token`() {
+    TODO()
+  }
+
+  @Test
+  fun `allows swagger without token`() {
+    TODO()
+  }
+}
