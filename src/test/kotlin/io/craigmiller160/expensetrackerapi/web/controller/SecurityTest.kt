cf209a13deb9bf3494cae484716ca604cbdc59b8
Craig Miller <craigmiller160@gmail.com>
2023-01-10 17:27:40 -0500

    Trying and failing to get the login to work

--- src/test/kotlin/io/craigmiller160/expensetrackerapi/web/controller/SecurityTest.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/web/controller/SecurityTest.kt
@@ -16,8 +16,8 @@ class SecurityTest(@Value("\${keycloak.auth-server-url}") private val authServer
         mapOf(
           "grant_type" to listOf("password"),
           "client_id" to listOf("expense-tracker-api"),
-          "username" to listOf("craig"),
-          "password" to listOf("password")))
+          "username" to listOf("craig@gmail.com"),
+          "password" to listOf("s3cr3t")))
     return restTemplate
       .postForEntity(
         "$authServerUrl/realms/apps-dev/protocol/openid-connect/token",
