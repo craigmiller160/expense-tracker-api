f475c6ae36785f4a1f0b2ee6075003b231b83f8f
Craig Miller <craigmiller160@gmail.com>
2023-01-10 16:22:06 -0500

    Working on terraform client config

---
+++ deploy/terraform/clients.tf
@@ -0,0 +1,22 @@
+data "keycloak_realm" "apps_dev" {
+  realm = "apps-dev"
+}
+
+data "keycloak_realm" "apps_prod" {
+  realm = "apps-prod"
+}
+
+locals {
+  client_common = {
+    client_id = "expense-tracker-api"
+    name = "expense-tracker-api"
+    enabled = true
+  }
+}
+
+resource "keycloak_openid_client" "expense_tracker_api" {
+  realm_id = data.keycloak_realm.apps_dev.id
+  client_id = local.client_common.client_id
+  name = local.client_common.name
+  enabled = local.client_common.enabled
+}
