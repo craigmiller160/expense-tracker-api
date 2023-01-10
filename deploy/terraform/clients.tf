cb7740168796d5ae7b228ed0abebde303d547c2d
Craig Miller <craigmiller160@gmail.com>
2023-01-10 16:26:01 -0500

    More progress with clients

--- deploy/terraform/clients.tf
+++ deploy/terraform/clients.tf
@@ -11,6 +11,8 @@ locals {
     client_id = "expense-tracker-api"
     name = "expense-tracker-api"
     enabled = true
+    access_type = "BEARER-ONLY"
+    service_accounts_enabled = true
   }
 }
 
@@ -19,4 +21,6 @@ resource "keycloak_openid_client" "expense_tracker_api" {
   client_id = local.client_common.client_id
   name = local.client_common.name
   enabled = local.client_common.enabled
+  access_type = local.client_common.access_type
+  service_accounts_enabled = local.client_common.service_accounts_enabled
 }
