8d51c3632f2354f8ee8d86d66d2890a8341dc6e2
Craig Miller <craigmiller160@gmail.com>
2023-01-11 09:47:50 -0500

    Disabled testcontainer and got app to run, there is some transitive conflict

--- src/test/kotlin/io/craigmiller160/expensetrackerapi/testcontainers/KeyCloakContainer.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/testcontainers/KeyCloakContainer.kt
@@ -1,17 +1,17 @@
 package io.craigmiller160.expensetrackerapi.testcontainers
 
-import dasniko.testcontainers.keycloak.KeycloakContainer
+// import dasniko.testcontainers.keycloak.KeycloakContainer
 
-class KeyCloakContainer : KeycloakContainer("quay.io/keycloak/keycloak:20.0.2") {
-  companion object {
-    val INSTANCE = KeyCloakContainer()
-  }
-  init {
-    withRealmImportFile("keycloak-realm.json")
-  }
-
-  override fun start() {
-    super.start()
-    System.setProperty("keycloak.auth-server-url", authServerUrl)
-  }
-}
+// class KeyCloakContainer : KeycloakContainer("quay.io/keycloak/keycloak:20.0.2") {
+//  companion object {
+//    val INSTANCE = KeyCloakContainer()
+//  }
+//  init {
+//    withRealmImportFile("keycloak-realm.json")
+//  }
+//
+//  override fun start() {
+//    super.start()
+//    System.setProperty("keycloak.auth-server-url", authServerUrl)
+//  }
+// }
