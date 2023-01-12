bfee5ed3f8fbd5d5689e8a366c709b72b5128f30
Craig Miller <craigmiller160@gmail.com>
2023-01-12 18:28:56 -0500

    Restored method

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/service/CategoryService.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/service/CategoryService.kt
@@ -5,9 +5,12 @@ import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.CategoryId
 import io.craigmiller160.expensetrackerapi.common.error.BadRequestException
 import io.craigmiller160.expensetrackerapi.data.constants.CategoryConstants
+import io.craigmiller160.expensetrackerapi.data.model.Category
 import io.craigmiller160.expensetrackerapi.data.repository.CategoryRepository
 import io.craigmiller160.expensetrackerapi.data.repository.TransactionRepository
 import io.craigmiller160.expensetrackerapi.function.TryEither
+import io.craigmiller160.expensetrackerapi.function.flatMapCatch
+import io.craigmiller160.expensetrackerapi.utils.StringToColor
 import io.craigmiller160.expensetrackerapi.web.types.category.CategoryRequest
 import io.craigmiller160.expensetrackerapi.web.types.category.CategoryResponse
 import org.springframework.stereotype.Service
@@ -37,16 +40,13 @@ class CategoryService(
 
   @Transactional
   fun createCategory(request: CategoryRequest): TryEither<CategoryResponse> {
-    authService.getAuthUserId()
-    //    val userId = oAuth2Service.getAuthenticatedUser().userId
-    //    return validateRequest(request)
-    //      .flatMapCatch {
-    //        categoryRepository.save(
-    //          Category(name = request.name, userId = userId, color =
-    // StringToColor.get(request.name)))
-    //      }
-    //      .map { CategoryResponse.from(it) }
-    TODO()
+    val userId = authService.getAuthUserId()
+    return validateRequest(request)
+      .flatMapCatch {
+        categoryRepository.save(
+          Category(name = request.name, userId = userId, color = StringToColor.get(request.name)))
+      }
+      .map { CategoryResponse.from(it) }
   }
 
   @Transactional
