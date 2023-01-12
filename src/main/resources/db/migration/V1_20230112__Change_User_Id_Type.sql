9afa72ec07d9dd172ea423c94565b853a8a4de1a
Craig Miller <craigmiller160@gmail.com>
2023-01-12 18:58:33 -0500

    More sql migration changes

--- src/main/resources/db/migration/V1_20230112__Change_User_Id_Type.sql
+++ src/main/resources/db/migration/V1_20230112__Change_User_Id_Type.sql
@@ -67,3 +67,33 @@ CREATE VIEW transactions_view(uid, user_id, expense_date, description, amount, c
    FROM transactions t1
      LEFT JOIN categories c ON t1.category_id = c.uid;
 
+DROP TRIGGER set_content_hash_on_change ON transactions;
+DROP FUNCTION set_content_hash_on_change();
+DROP FUNCTION txn_content_hash(user_id UUID, expense_date DATE, amount NUMERIC, description VARCHAR(255));
+
+CREATE FUNCTION txn_content_hash (user_id UUID, expense_date DATE, amount NUMERIC,
+    description VARCHAR(255), mark_not_duplicate_nano BIGINT)
+RETURNS VARCHAR AS
+$$
+    BEGIN
+        RETURN ENCODE(DIGEST(CONCAT(user_id, expense_date, amount, description, mark_not_duplicate_nano), 'sha256'), 'hex');
+    END;
+$$ LANGUAGE plpgsql;
+
+UPDATE transactions
+SET content_hash = txn_content_hash(user_id, expense_date, amount, description, mark_not_duplicate_nano);
+
+CREATE FUNCTION set_content_hash_on_change()
+RETURNS TRIGGER AS
+$$
+    BEGIN
+        new.content_hash := txn_content_hash(new.user_id, new.expense_date, new.amount, new.description, new.mark_not_duplicate_nano);
+        RETURN new;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_content_hash_on_change
+BEFORE INSERT OR UPDATE ON transactions
+FOR EACH ROW
+EXECUTE PROCEDURE set_content_hash_on_change();
+
