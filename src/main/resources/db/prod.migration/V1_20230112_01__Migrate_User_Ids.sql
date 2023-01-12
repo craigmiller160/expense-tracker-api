ee211dbbaf3256d14c8b1e61b7dbd9275df2906f
Craig Miller <craigmiller160@gmail.com>
2023-01-12 17:55:30 -0500

    Added prod data migration

---
+++ src/main/resources/db/prod.migration/V1_20230112_01__Migrate_User_Ids.sql
@@ -0,0 +1,39 @@
+UPDATE transactions
+SET user_id = '7a8e0603-dca6-474c-a0f7-34a748567aee'
+WHERE legacy_user_id = 1;
+
+ALTER TABLE transactions
+DROP COLUMN legacy_user_id;
+
+ALTER TABLE transactions
+ALTER COLUMN user_id SET NOT NULL;
+
+UPDATE auto_categorize_rules
+SET user_id = '7a8e0603-dca6-474c-a0f7-34a748567aee'
+WHERE legacy_user_id = 1;
+
+ALTER TABLE auto_categorize_rules
+DROP COLUMN legacy_user_id;
+
+ALTER TABLE auto_categorize_rules
+ALTER COLUMN user_id SET NOT NULL;
+
+UPDATE categories
+SET user_id = '7a8e0603-dca6-474c-a0f7-34a748567aee'
+WHERE legacy_user_id = 1;
+
+ALTER TABLE categories
+DROP COLUMN legacy_user_id;
+
+ALTER TABLE categories
+ALTER COLUMN user_id SET NOT NULL;
+
+UPDATE last_rule_applied
+SET user_id = '7a8e0603-dca6-474c-a0f7-34a748567aee'
+WHERE legacy_user_id = 1;
+
+ALTER TABLE last_rule_applied
+DROP COLUMN legacy_user_id;
+
+ALTER TABLE last_rule_applied
+ALTER COLUMN user_id SET NOT NULL;
