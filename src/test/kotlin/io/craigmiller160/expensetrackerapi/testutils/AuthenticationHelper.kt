29035b19f4e87119f7156b40159b1fbff468933c
Craig Miller <craigmiller160@gmail.com>
2023-01-12 17:11:09 -0500

    Working on test access setup logic

--- src/test/kotlin/io/craigmiller160/expensetrackerapi/testutils/AuthenticationHelper.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/testutils/AuthenticationHelper.kt
@@ -1,11 +1,16 @@
 package io.craigmiller160.expensetrackerapi.testutils
 
+import javax.annotation.PostConstruct
 import org.keycloak.adapters.KeycloakConfigResolver
 import org.keycloak.admin.client.Keycloak
+import org.keycloak.representations.idm.UserRepresentation
 import org.springframework.stereotype.Component
 
 @Component
 class AuthenticationHelper(configResolver: KeycloakConfigResolver) {
+  companion object {
+    const val ROLE_ACCESS = "access"
+  }
   private val keycloakDeployment = configResolver.resolve(null)
   private val keycloak =
     Keycloak.getInstance(
@@ -15,4 +20,20 @@ class AuthenticationHelper(configResolver: KeycloakConfigResolver) {
       "admin",
       keycloakDeployment.resourceName,
       keycloakDeployment.resourceCredentials["secret"].toString())
+
+  fun createUser(userName: String, roles: List<String> = listOf(ROLE_ACCESS)) {
+    val user =
+      UserRepresentation().apply {
+        username = userName
+        isEnabled = true
+        isEmailVerified = true
+        firstName = "First $userName"
+        lastName = "Last $userName"
+        email = userName
+        realmRoles = roles
+      }
+    keycloak.realm(keycloakDeployment.realm).users().create(user)
+  }
+
+  @PostConstruct fun setup() {}
 }
