1d198f2a29bf2eb2580de554f9a1fac7a9a5fa1e
Craig Miller <craigmiller160@gmail.com>
2023-01-12 17:16:44 -0500

    Working more on user setup

--- src/test/kotlin/io/craigmiller160/expensetrackerapi/testutils/AuthenticationHelper.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/testutils/AuthenticationHelper.kt
@@ -1,8 +1,11 @@
 package io.craigmiller160.expensetrackerapi.testutils
 
+import java.util.concurrent.ConcurrentHashMap
 import javax.annotation.PostConstruct
 import org.keycloak.adapters.KeycloakConfigResolver
-import org.keycloak.admin.client.Keycloak
+import org.keycloak.admin.client.CreatedResponseUtil
+import org.keycloak.admin.client.KeycloakBuilder
+import org.keycloak.representations.idm.CredentialRepresentation
 import org.keycloak.representations.idm.UserRepresentation
 import org.springframework.stereotype.Component
 
@@ -11,15 +14,19 @@ class AuthenticationHelper(configResolver: KeycloakConfigResolver) {
   companion object {
     const val ROLE_ACCESS = "access"
   }
+
   private val keycloakDeployment = configResolver.resolve(null)
   private val keycloak =
-    Keycloak.getInstance(
-      keycloakDeployment.authServerBaseUrl,
-      keycloakDeployment.realm,
-      "admin",
-      "admin",
-      keycloakDeployment.resourceName,
-      keycloakDeployment.resourceCredentials["secret"].toString())
+    KeycloakBuilder.builder()
+      .serverUrl(keycloakDeployment.authServerBaseUrl)
+      .realm("master")
+      .clientId("admin-cli")
+      .grantType("password")
+      .username("admin")
+      .password("admin")
+      .build()
+
+  private val defaultUsers = ConcurrentHashMap<String, String>()
 
   fun createUser(userName: String, roles: List<String> = listOf(ROLE_ACCESS)) {
     val user =
@@ -32,8 +39,21 @@ class AuthenticationHelper(configResolver: KeycloakConfigResolver) {
         email = userName
         realmRoles = roles
       }
-    keycloak.realm(keycloakDeployment.realm).users().create(user)
+    val response = keycloak.realm(keycloakDeployment.realm).users().create(user)
+    val userId = CreatedResponseUtil.getCreatedId(response)
+
+    val passwordCred =
+      CredentialRepresentation().apply {
+        isTemporary = false
+        type = CredentialRepresentation.PASSWORD
+        value = "password"
+      }
+
+    keycloak.realm(keycloakDeployment.realm).users().get(userId).resetPassword(passwordCred)
   }
 
-  @PostConstruct fun setup() {}
+  @PostConstruct
+  fun setup() {
+    createUser("myUser@gmail.com")
+  }
 }
