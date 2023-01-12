3c2813ce60a151e7fb133226f5dda17846e2349a
Craig Miller <craigmiller160@gmail.com>
2023-01-12 17:48:08 -0500

    More work on sql migration

--- src/main/resources/db/migration/V1_20230112__Change_User_Id_Type.sql
+++ src/main/resources/db/migration/V1_20230112__Change_User_Id_Type.sql
@@ -1,7 +1,25 @@
--- Transaction, AutoCategorizeRule, AutoCategorizeRuleView, Category, LastRuleApplied, TransactionView
+-- AutoCategorizeRuleView, TransactionView
 
 ALTER TABLE transactions
 RENAME COLUMN user_id TO legacy_user_id;
 
 ALTER TABLE transactions
+ADD COLUMN user_id UUID;
+
+ALTER TABLE auto_categorize_rules
+RENAME COLUMN user_id TO legacy_user_id;
+
+ALTER TABLE auto_categorize_rules
+ADD COLUMN user_id UUID;
+
+ALTER TABLE categories
+RENAME COLUMN user_id TO legacy_user_id;
+
+ALTER TABLE categories
+ADD COLUMN user_id UUID;
+
+ALTER TABLE last_rule_applied
+RENAME COLUMN user_id TO legacy_user_id;
+
+ALTER TABLE last_rule_applied
 ADD COLUMN user_id UUID;
