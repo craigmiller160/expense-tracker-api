d150ba1a29bcb874403a8935f0d2d75992923629
Craig Miller <craigmiller160@gmail.com>
2023-01-12 17:36:26 -0500

    Implemented another test

--- src/test/kotlin/io/craigmiller160/expensetrackerapi/web/controller/SecurityTest.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/web/controller/SecurityTest.kt
@@ -4,20 +4,13 @@ import io.craigmiller160.expensetrackerapi.testcore.ExpenseTrackerIntegrationTes
 import io.craigmiller160.expensetrackerapi.testutils.AuthenticationHelper
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 
 @ExpenseTrackerIntegrationTest
 class SecurityTest
 @Autowired
-constructor(
-  @Value("\${keycloak.auth-server-url}") private val authServerUrl: String,
-  @Value("\${keycloak.resource}") private val clientId: String,
-  @Value("\${keycloak.credentials.secret}") private val clientSecret: String,
-  private val mockMvc: MockMvc,
-  private val authHelper: AuthenticationHelper
-) {
+constructor(private val mockMvc: MockMvc, private val authHelper: AuthenticationHelper) {
   @Test
   fun `allows valid token with access role`() {
     val token = authHelper.primaryUser.token
@@ -32,6 +25,13 @@ constructor(
   @Test
   fun `rejects valid token without access role`() {
     TODO()
+  }
+
+  @Test
+  fun `rejects no token`() {
+    mockMvc
+      .get("/transaction-import/types") { secure = true }
+      .andExpect { status { isUnauthorized() } }
   }
 
   @Test
