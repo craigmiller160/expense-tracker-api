ecb578ffddea4fbc28cc329105c13683c271b3f8
Craig Miller <craigmiller160@gmail.com>
2023-01-13 10:09:08 -0500

    Restored test logic

--- src/test/kotlin/io/craigmiller160/expensetrackerapi/service/ApplyCategoriesToTransactionsServiceTest.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/service/ApplyCategoriesToTransactionsServiceTest.kt
@@ -5,15 +5,20 @@ import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.AutoCategoriz
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.CategoryId
 import io.craigmiller160.expensetrackerapi.data.model.AutoCategorizeRule
 import io.craigmiller160.expensetrackerapi.data.model.Category
+import io.craigmiller160.expensetrackerapi.data.model.LastRuleApplied
 import io.craigmiller160.expensetrackerapi.data.model.Transaction
 import io.craigmiller160.expensetrackerapi.data.repository.AutoCategorizeRuleRepository
 import io.craigmiller160.expensetrackerapi.data.repository.LastRuleAppliedRepository
 import io.craigmiller160.expensetrackerapi.data.repository.TransactionRepository
+import io.craigmiller160.expensetrackerapi.extension.flushAndClear
 import io.craigmiller160.expensetrackerapi.testcore.ExpenseTrackerIntegrationTest
+import io.craigmiller160.expensetrackerapi.testutils.AuthenticationHelper
 import io.craigmiller160.expensetrackerapi.testutils.DataHelper
+import io.kotest.assertions.arrow.core.shouldBeRight
 import java.math.BigDecimal
 import java.time.LocalDate
 import javax.persistence.EntityManager
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -27,7 +32,8 @@ constructor(
   private val transactionRepository: TransactionRepository,
   private val applyCategoriesToTransactionsService: ApplyCategoriesToTransactionsService,
   private val lastRuleAppliedRepository: LastRuleAppliedRepository,
-  private val entityManager: EntityManager
+  private val entityManager: EntityManager,
+  private val authHelper: AuthenticationHelper
 ) {
 
   private var ruleCounter = 0
@@ -39,53 +45,51 @@ constructor(
   @BeforeEach
   fun setup() {
     ruleCounter = 0
-    //    val cat0 = dataHelper.createCategory(1L, "Entertainment")
-    //    val cat1 = dataHelper.createCategory(1L, "Food")
-    //    val cat2 = dataHelper.createCategory(1L, "Bills")
-    //    val cat3 = dataHelper.createCategory(1L, "Other")
-    //    val cat4 = dataHelper.createCategory(1L, "Something")
-    //    val cat5 = dataHelper.createCategory(1L, "Foo")
-    //    val cat6 = dataHelper.createCategory(1L, "To Somewhere")
-    //
-    //    categories = listOf(cat0, cat1, cat2, cat3, cat4, cat5, cat6)
-    //
-    //    val txn0 = createTransaction("Target", LocalDate.of(2022, 1, 1), BigDecimal("10"))
-    //    val txn1 = createTransaction("Target", LocalDate.of(2022, 1, 1), BigDecimal("100"))
-    //    val txn2 = createTransaction("AMC Theaters", LocalDate.of(2022, 3, 10), BigDecimal("22"))
-    //    val txn3 = createTransaction("AMC Theaters", LocalDate.of(2022, 6, 1), BigDecimal("10"))
-    //    val txn4 = createTransaction("AMC Theaters 2", LocalDate.of(2022, 12, 10),
-    // BigDecimal("22"))
-    //    val txn5 = createTransaction("Target", LocalDate.of(2022, 1, 1), BigDecimal("20"))
-    //    val txn6 =
-    //      createTransaction("Universe", LocalDate.now(), BigDecimal("10")).let {
-    //        transactionRepository.save(it.apply { categoryId = cat6.uid })
-    //      }
-    //
-    //    transactions = listOf(txn0, txn1, txn2, txn3, txn4, txn5, txn6)
-    //
-    //    val rule0 = createRule(categoryId = cat2.uid, regex = "Target")
-    //    val rule1 = createRule(categoryId = cat0.uid, regex = "Target", minAmount =
-    // BigDecimal("90"))
-    //    val rule2 = createRule(categoryId = cat1.uid, regex = "Target", maxAmount =
-    // BigDecimal("15"))
-    //    val rule3 = createRule(categoryId = cat5.uid, regex = "AMC.*")
-    //    val rule4 =
-    //      createRule(categoryId = cat3.uid, regex = "AMC.*", startDate = LocalDate.of(2022, 11,
-    // 1))
-    //    val rule5 =
-    //      createRule(categoryId = cat4.uid, regex = "AMC.*", endDate = LocalDate.of(2022, 4, 1))
-    //    rules = listOf(rule0, rule1, rule2, rule3, rule4, rule5)
-    TODO()
+    val cat0 = dataHelper.createCategory(authHelper.primaryUser.userId, "Entertainment")
+    val cat1 = dataHelper.createCategory(authHelper.primaryUser.userId, "Food")
+    val cat2 = dataHelper.createCategory(authHelper.primaryUser.userId, "Bills")
+    val cat3 = dataHelper.createCategory(authHelper.primaryUser.userId, "Other")
+    val cat4 = dataHelper.createCategory(authHelper.primaryUser.userId, "Something")
+    val cat5 = dataHelper.createCategory(authHelper.primaryUser.userId, "Foo")
+    val cat6 = dataHelper.createCategory(authHelper.primaryUser.userId, "To Somewhere")
+
+    categories = listOf(cat0, cat1, cat2, cat3, cat4, cat5, cat6)
+
+    val txn0 = createTransaction("Target", LocalDate.of(2022, 1, 1), BigDecimal("10"))
+    val txn1 = createTransaction("Target", LocalDate.of(2022, 1, 1), BigDecimal("100"))
+    val txn2 = createTransaction("AMC Theaters", LocalDate.of(2022, 3, 10), BigDecimal("22"))
+    val txn3 = createTransaction("AMC Theaters", LocalDate.of(2022, 6, 1), BigDecimal("10"))
+    val txn4 = createTransaction("AMC Theaters 2", LocalDate.of(2022, 12, 10), BigDecimal("22"))
+    val txn5 = createTransaction("Target", LocalDate.of(2022, 1, 1), BigDecimal("20"))
+    val txn6 =
+      createTransaction("Universe", LocalDate.now(), BigDecimal("10")).let {
+        transactionRepository.save(it.apply { categoryId = cat6.uid })
+      }
+
+    transactions = listOf(txn0, txn1, txn2, txn3, txn4, txn5, txn6)
+
+    val rule0 = createRule(categoryId = cat2.uid, regex = "Target")
+    val rule1 = createRule(categoryId = cat0.uid, regex = "Target", minAmount = BigDecimal("90"))
+    val rule2 = createRule(categoryId = cat1.uid, regex = "Target", maxAmount = BigDecimal("15"))
+    val rule3 = createRule(categoryId = cat5.uid, regex = "AMC.*")
+    val rule4 =
+      createRule(categoryId = cat3.uid, regex = "AMC.*", startDate = LocalDate.of(2022, 11, 1))
+    val rule5 =
+      createRule(categoryId = cat4.uid, regex = "AMC.*", endDate = LocalDate.of(2022, 4, 1))
+    rules = listOf(rule0, rule1, rule2, rule3, rule4, rule5)
   }
 
   private fun createTransaction(
     description: String,
     expenseDate: LocalDate,
     amount: BigDecimal
-  ): Transaction = TODO()
-  //    transactionRepository.save(
-  //      Transaction(
-  //        userId = 1L, expenseDate = expenseDate, description = description, amount = amount))
+  ): Transaction =
+    transactionRepository.save(
+      Transaction(
+        userId = authHelper.primaryUser.userId,
+        expenseDate = expenseDate,
+        description = description,
+        amount = amount))
 
   private fun createRule(
     categoryId: TypedId<CategoryId>,
@@ -94,38 +98,39 @@ constructor(
     endDate: LocalDate? = null,
     minAmount: BigDecimal? = null,
     maxAmount: BigDecimal? = null
-  ): AutoCategorizeRule = TODO()
-  //    autoCategorizeRuleRepository.save(
-  //      AutoCategorizeRule(
-  //        userId = 1L,
-  //        categoryId = categoryId,
-  //        ordinal = ++ruleCounter,
-  //        regex = regex,
-  //        startDate = startDate,
-  //        endDate = endDate,
-  //        minAmount = minAmount,
-  //        maxAmount = maxAmount))
+  ): AutoCategorizeRule =
+    autoCategorizeRuleRepository.save(
+      AutoCategorizeRule(
+        userId = authHelper.primaryUser.userId,
+        categoryId = categoryId,
+        ordinal = ++ruleCounter,
+        regex = regex,
+        startDate = startDate,
+        endDate = endDate,
+        minAmount = minAmount,
+        maxAmount = maxAmount))
 
   @Test
   fun applyCategoriesToTransactions() {
-    //    lastRuleAppliedRepository.saveAndFlush(
-    //      LastRuleApplied(userId = 1L, ruleId = rules[0].uid, transactionId =
-    // transactions[6].uid))
-    //    val result =
-    //      applyCategoriesToTransactionsService
-    //        .applyCategoriesToTransactions(1L, transactions)
-    //        .shouldBeRight()
-    //
-    //    entityManager.flushAndClear()
-    //
-    //    validateCategoryApplied(transactions[1], categories[0].uid, rules[1].uid)
-    //    validateCategoryApplied(transactions[0], categories[1].uid, rules[2].uid)
-    //    validateCategoryApplied(transactions[5], categories[2].uid, rules[0].uid)
-    //    validateCategoryApplied(transactions[4], categories[3].uid, rules[4].uid)
-    //    validateCategoryApplied(transactions[2], categories[4].uid, rules[5].uid)
-    //    validateCategoryApplied(transactions[3], categories[5].uid, rules[3].uid)
-    //    validateCategoryApplied(transactions[6], null, null)
-    TODO()
+    lastRuleAppliedRepository.saveAndFlush(
+      LastRuleApplied(
+        userId = authHelper.primaryUser.userId,
+        ruleId = rules[0].uid,
+        transactionId = transactions[6].uid))
+    val result =
+      applyCategoriesToTransactionsService
+        .applyCategoriesToTransactions(authHelper.primaryUser.userId, transactions)
+        .shouldBeRight()
+
+    entityManager.flushAndClear()
+
+    validateCategoryApplied(transactions[1], categories[0].uid, rules[1].uid)
+    validateCategoryApplied(transactions[0], categories[1].uid, rules[2].uid)
+    validateCategoryApplied(transactions[5], categories[2].uid, rules[0].uid)
+    validateCategoryApplied(transactions[4], categories[3].uid, rules[4].uid)
+    validateCategoryApplied(transactions[2], categories[4].uid, rules[5].uid)
+    validateCategoryApplied(transactions[3], categories[5].uid, rules[3].uid)
+    validateCategoryApplied(transactions[6], null, null)
   }
 
   private fun validateCategoryApplied(
@@ -133,17 +138,20 @@ constructor(
     categoryId: TypedId<CategoryId>?,
     ruleId: TypedId<AutoCategorizeRuleId>?
   ) {
-    //    assertThat(transactionRepository.findById(txn.uid))
-    //      .isPresent
-    //      .get()
-    //      .hasFieldOrPropertyWithValue("categoryId", categoryId)
-    //    ruleId?.let { nonNullRuleId ->
-    //      assertThat(lastRuleAppliedRepository.findByUserIdAndTransactionId(1L, txn.uid))
-    //        .isNotNull
-    //        .hasFieldOrPropertyWithValue("ruleId", nonNullRuleId)
-    //    }
-    //      ?: assertThat(lastRuleAppliedRepository.findByUserIdAndTransactionId(1L,
-    // txn.uid)).isNull()
-    TODO()
+    assertThat(transactionRepository.findById(txn.uid))
+      .isPresent
+      .get()
+      .hasFieldOrPropertyWithValue("categoryId", categoryId)
+    ruleId?.let { nonNullRuleId ->
+      assertThat(
+          lastRuleAppliedRepository.findByUserIdAndTransactionId(
+            authHelper.primaryUser.userId, txn.uid))
+        .isNotNull
+        .hasFieldOrPropertyWithValue("ruleId", nonNullRuleId)
+    }
+      ?: assertThat(
+          lastRuleAppliedRepository.findByUserIdAndTransactionId(
+            authHelper.primaryUser.userId, txn.uid))
+        .isNull()
   }
 }
