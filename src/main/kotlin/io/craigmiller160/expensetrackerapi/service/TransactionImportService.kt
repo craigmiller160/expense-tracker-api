33c79bc09f3c2b1f3a5daf6286072fd7c0f6525b
Craig Miller <craigmiller160@gmail.com>
2023-01-12 21:38:43 -0500

    Restored function

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/service/TransactionImportService.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/service/TransactionImportService.kt
@@ -1,7 +1,9 @@
 package io.craigmiller160.expensetrackerapi.service
 
+import arrow.core.flatMap
 import io.craigmiller160.expensetrackerapi.data.repository.TransactionRepository
 import io.craigmiller160.expensetrackerapi.function.TryEither
+import io.craigmiller160.expensetrackerapi.function.flatMapCatch
 import io.craigmiller160.expensetrackerapi.service.parsing.TransactionParserManager
 import io.craigmiller160.expensetrackerapi.web.types.importing.ImportTransactionsResponse
 import io.craigmiller160.expensetrackerapi.web.types.importing.ImportTypeResponse
@@ -13,7 +15,8 @@ import org.springframework.stereotype.Service
 class TransactionImportService(
   private val transactionRepository: TransactionRepository,
   private val transactionParserManager: TransactionParserManager,
-  private val applyCategoriesToTransactionsService: ApplyCategoriesToTransactionsService
+  private val applyCategoriesToTransactionsService: ApplyCategoriesToTransactionsService,
+  private val authService: AuthorizationService
 ) {
   fun getImportTypes(): List<ImportTypeResponse> =
     TransactionImportType.values().map { ImportTypeResponse(it.name, it.displayName) }
@@ -24,15 +27,13 @@ class TransactionImportService(
     stream: InputStream
   ): TryEither<ImportTransactionsResponse> {
     val parser = transactionParserManager.getParserForType(type)
-    //    val userId = oAuth2Service.getAuthenticatedUser().userId
-    //    return parser
-    //      .parse(userId, stream)
-    //      .flatMapCatch { transactions -> transactionRepository.saveAll(transactions) }
-    //      .flatMap { transactions ->
-    //        applyCategoriesToTransactionsService.applyCategoriesToTransactions(userId,
-    // transactions)
-    //      }
-    //      .map { transactions -> ImportTransactionsResponse(transactions.size) }
-    TODO()
+    val userId = authService.getAuthUserId()
+    return parser
+      .parse(userId, stream)
+      .flatMapCatch { transactions -> transactionRepository.saveAll(transactions) }
+      .flatMap { transactions ->
+        applyCategoriesToTransactionsService.applyCategoriesToTransactions(userId, transactions)
+      }
+      .map { transactions -> ImportTransactionsResponse(transactions.size) }
   }
 }
