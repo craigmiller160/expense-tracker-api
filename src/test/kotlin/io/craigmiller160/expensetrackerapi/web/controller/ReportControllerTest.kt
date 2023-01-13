332201c24bf41c21c316c385ee4a3f86236a3b24
Craig Miller <craigmiller160@gmail.com>
2023-01-12 19:06:48 -0500

    Restored test logic

--- src/test/kotlin/io/craigmiller160/expensetrackerapi/web/controller/ReportControllerTest.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/web/controller/ReportControllerTest.kt
@@ -1,13 +1,18 @@
 package io.craigmiller160.expensetrackerapi.web.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.craigmiller160.expensetrackerapi.data.constants.CategoryConstants
 import io.craigmiller160.expensetrackerapi.data.model.Category
 import io.craigmiller160.expensetrackerapi.data.model.Transaction
 import io.craigmiller160.expensetrackerapi.data.repository.TransactionRepository
 import io.craigmiller160.expensetrackerapi.extension.flushAndClear
 import io.craigmiller160.expensetrackerapi.testcore.ExpenseTrackerIntegrationTest
+import io.craigmiller160.expensetrackerapi.testutils.AuthenticationHelper
 import io.craigmiller160.expensetrackerapi.testutils.DataHelper
+import io.craigmiller160.expensetrackerapi.web.types.report.ReportCategoryResponse
+import io.craigmiller160.expensetrackerapi.web.types.report.ReportMonthResponse
 import io.craigmiller160.expensetrackerapi.web.types.report.ReportPageResponse
+import java.time.LocalDate
 import javax.persistence.EntityManager
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -23,7 +28,8 @@ constructor(
   private val objectMapper: ObjectMapper,
   private val dataHelper: DataHelper,
   private val transactionRepository: TransactionRepository,
-  private val entityManager: EntityManager
+  private val entityManager: EntityManager,
+  private val authHelper: AuthenticationHelper
 ) {
   private lateinit var token: String
 
@@ -33,107 +39,106 @@ constructor(
 
   @BeforeEach
   fun setup() {
-    //    token = OAuth2Extension.createJwt()
-    //    val cat1 = dataHelper.createCategory(1L, "Entertainment")
-    //    val cat2 = dataHelper.createCategory(1L, "Groceries")
-    //    val cat3 = dataHelper.createCategory(2L, "Food")
-    //    val cat4 = dataHelper.createCategory(1L, "Restaurants")
-    //    val cat5 = dataHelper.createCategory(1L, "Travel")
-    //    categories = listOf(cat1, cat2, cat3, cat4, cat5)
-    //
-    //    val month1 = LocalDate.of(2022, 1, 1)
-    //    val month2 = LocalDate.of(2022, 2, 1)
-    //
-    //    val txn1 =
-    //      dataHelper.createTransaction(1L, cat1.uid).let {
-    //        transactionRepository.save(it.apply { expenseDate = month1.plusDays(1) })
-    //      }
-    //    val txn2 =
-    //      dataHelper.createTransaction(1L, cat2.uid).let {
-    //        transactionRepository.save(it.apply { expenseDate = month1.plusDays(2) })
-    //      }
-    //    val txn3 =
-    //      dataHelper.createTransaction(1L, cat1.uid).let {
-    //        transactionRepository.save(it.apply { expenseDate = month2.plusDays(3) })
-    //      }
-    //    val txn4 =
-    //      dataHelper.createTransaction(1L, cat2.uid).let {
-    //        transactionRepository.save(it.apply { expenseDate = month2.plusDays(4) })
-    //      }
-    //    val txn5 =
-    //      dataHelper.createTransaction(2L, cat3.uid).let {
-    //        transactionRepository.save(it.apply { expenseDate = month1.plusDays(5) })
-    //      }
-    //    val txn6 =
-    //      dataHelper.createTransaction(1L).let {
-    //        transactionRepository.save(it.apply { expenseDate = month1.plusDays(6) })
-    //      }
-    //    val txn7 =
-    //      dataHelper.createTransaction(1L, cat5.uid).let {
-    //        transactionRepository.save(it.apply { expenseDate = month1.plusDays(7) })
-    //      }
-    //    val txn8 =
-    //      dataHelper.createTransaction(1L, cat4.uid).let {
-    //        transactionRepository.save(it.apply { expenseDate = month1.plusDays(8) })
-    //      }
-    //    transactions = listOf(txn1, txn2, txn3, txn4, txn5, txn6, txn7, txn8)
-    //
-    //    entityManager.flushAndClear()
-    //
-    //    val month1Total = txn1.amount + txn2.amount + txn6.amount + txn7.amount + txn8.amount
-    //    val month2Total = txn3.amount + txn4.amount
-    //    expectedResponse =
-    //      ReportPageResponse(
-    //        pageNumber = 0,
-    //        totalItems = 2,
-    //        reports =
-    //          listOf(
-    //            ReportMonthResponse(
-    //              date = month2,
-    //              total = month2Total,
-    //              categories =
-    //                listOf(
-    //                  ReportCategoryResponse(
-    //                    name = cat1.name,
-    //                    color = cat1.color,
-    //                    amount = txn3.amount,
-    //                    percent = txn3.amount / month2Total),
-    //                  ReportCategoryResponse(
-    //                    name = cat2.name,
-    //                    color = cat2.color,
-    //                    amount = txn4.amount,
-    //                    percent = txn4.amount / month2Total))),
-    //            ReportMonthResponse(
-    //              date = month1,
-    //              total = month1Total,
-    //              categories =
-    //                listOf(
-    //                  ReportCategoryResponse(
-    //                    name = cat1.name,
-    //                    color = cat1.color,
-    //                    amount = txn1.amount,
-    //                    percent = txn1.amount / month1Total),
-    //                  ReportCategoryResponse(
-    //                    name = cat2.name,
-    //                    color = cat2.color,
-    //                    amount = txn2.amount,
-    //                    percent = txn2.amount / month1Total),
-    //                  ReportCategoryResponse(
-    //                    name = cat4.name,
-    //                    color = cat4.color,
-    //                    amount = txn8.amount,
-    //                    percent = txn8.amount / month1Total),
-    //                  ReportCategoryResponse(
-    //                    name = cat5.name,
-    //                    color = cat5.color,
-    //                    amount = txn7.amount,
-    //                    percent = txn7.amount / month1Total),
-    //                  ReportCategoryResponse(
-    //                    name = CategoryConstants.UNKNOWN_CATEGORY_NAME,
-    //                    color = CategoryConstants.UNKNOWN_CATEGORY_COLOR,
-    //                    amount = txn6.amount,
-    //                    percent = txn6.amount / month1Total)))))
-    TODO()
+    token = authHelper.primaryUser.token
+    val cat1 = dataHelper.createCategory(authHelper.primaryUser.userId, "Entertainment")
+    val cat2 = dataHelper.createCategory(authHelper.primaryUser.userId, "Groceries")
+    val cat3 = dataHelper.createCategory(authHelper.secondaryUser.userId, "Food")
+    val cat4 = dataHelper.createCategory(authHelper.primaryUser.userId, "Restaurants")
+    val cat5 = dataHelper.createCategory(authHelper.primaryUser.userId, "Travel")
+    categories = listOf(cat1, cat2, cat3, cat4, cat5)
+
+    val month1 = LocalDate.of(2022, 1, 1)
+    val month2 = LocalDate.of(2022, 2, 1)
+
+    val txn1 =
+      dataHelper.createTransaction(authHelper.primaryUser.userId, cat1.uid).let {
+        transactionRepository.save(it.apply { expenseDate = month1.plusDays(1) })
+      }
+    val txn2 =
+      dataHelper.createTransaction(authHelper.primaryUser.userId, cat2.uid).let {
+        transactionRepository.save(it.apply { expenseDate = month1.plusDays(2) })
+      }
+    val txn3 =
+      dataHelper.createTransaction(authHelper.primaryUser.userId, cat1.uid).let {
+        transactionRepository.save(it.apply { expenseDate = month2.plusDays(3) })
+      }
+    val txn4 =
+      dataHelper.createTransaction(authHelper.primaryUser.userId, cat2.uid).let {
+        transactionRepository.save(it.apply { expenseDate = month2.plusDays(4) })
+      }
+    val txn5 =
+      dataHelper.createTransaction(authHelper.secondaryUser.userId, cat3.uid).let {
+        transactionRepository.save(it.apply { expenseDate = month1.plusDays(5) })
+      }
+    val txn6 =
+      dataHelper.createTransaction(authHelper.primaryUser.userId).let {
+        transactionRepository.save(it.apply { expenseDate = month1.plusDays(6) })
+      }
+    val txn7 =
+      dataHelper.createTransaction(authHelper.primaryUser.userId, cat5.uid).let {
+        transactionRepository.save(it.apply { expenseDate = month1.plusDays(7) })
+      }
+    val txn8 =
+      dataHelper.createTransaction(authHelper.primaryUser.userId, cat4.uid).let {
+        transactionRepository.save(it.apply { expenseDate = month1.plusDays(8) })
+      }
+    transactions = listOf(txn1, txn2, txn3, txn4, txn5, txn6, txn7, txn8)
+
+    entityManager.flushAndClear()
+
+    val month1Total = txn1.amount + txn2.amount + txn6.amount + txn7.amount + txn8.amount
+    val month2Total = txn3.amount + txn4.amount
+    expectedResponse =
+      ReportPageResponse(
+        pageNumber = 0,
+        totalItems = 2,
+        reports =
+          listOf(
+            ReportMonthResponse(
+              date = month2,
+              total = month2Total,
+              categories =
+                listOf(
+                  ReportCategoryResponse(
+                    name = cat1.name,
+                    color = cat1.color,
+                    amount = txn3.amount,
+                    percent = txn3.amount / month2Total),
+                  ReportCategoryResponse(
+                    name = cat2.name,
+                    color = cat2.color,
+                    amount = txn4.amount,
+                    percent = txn4.amount / month2Total))),
+            ReportMonthResponse(
+              date = month1,
+              total = month1Total,
+              categories =
+                listOf(
+                  ReportCategoryResponse(
+                    name = cat1.name,
+                    color = cat1.color,
+                    amount = txn1.amount,
+                    percent = txn1.amount / month1Total),
+                  ReportCategoryResponse(
+                    name = cat2.name,
+                    color = cat2.color,
+                    amount = txn2.amount,
+                    percent = txn2.amount / month1Total),
+                  ReportCategoryResponse(
+                    name = cat4.name,
+                    color = cat4.color,
+                    amount = txn8.amount,
+                    percent = txn8.amount / month1Total),
+                  ReportCategoryResponse(
+                    name = cat5.name,
+                    color = cat5.color,
+                    amount = txn7.amount,
+                    percent = txn7.amount / month1Total),
+                  ReportCategoryResponse(
+                    name = CategoryConstants.UNKNOWN_CATEGORY_NAME,
+                    color = CategoryConstants.UNKNOWN_CATEGORY_COLOR,
+                    amount = txn6.amount,
+                    percent = txn6.amount / month1Total)))))
   }
 
   @Test
