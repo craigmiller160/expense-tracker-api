259342bc7e20e86e4879a40b9fa2c113051b3389
Craig Miller <craigmiller160@gmail.com>
2023-01-19 16:16:33 -0500

    Removed old oauth2 coode

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/config/OAuth2UtilsConfig.kt
+++
@@ -1,8 +0,0 @@
-package io.craigmiller160.expensetrackerapi.config
-
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-@ComponentScan(basePackages = ["io.craigmiller160.spring.oauth2", "io.craigmiller160.oauth2"])
-class OAuth2UtilsConfig
