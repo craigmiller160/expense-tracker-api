7009d69aa7d0f2ce9e697037a3117f6b415f0289
Craig Miller <craigmiller160@gmail.com>
2023-01-25 12:36:20 -0500

    Updated usernames

--- src/test/kotlin/io/craigmiller160/expensetrackerapi/testutils/AuthenticationHelperConfig.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/testutils/AuthenticationHelperConfig.kt
@@ -1,7 +1,6 @@
 package io.craigmiller160.expensetrackerapi.testutils
 
 import io.craigmiller160.testcontainers.common.core.AuthenticationHelper
-import java.util.UUID
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -14,11 +13,9 @@ class AuthenticationHelperConfig {
 
   @Bean
   fun defaultUsers(authHelper: AuthenticationHelper): DefaultUsers {
-    val id = UUID.randomUUID().toString()
-    val primaryUser = authHelper.createUser("primary_$id@gmail.com").let { authHelper.login(it) }
-    val secondaryUser =
-      authHelper.createUser("secondary_$id@gmail.com").let { authHelper.login(it) }
-    val tertiaryUser = authHelper.createUser("tertiary_$id@gmail.com").let { authHelper.login(it) }
+    val primaryUser = authHelper.createUser("primary@gmail.com").let { authHelper.login(it) }
+    val secondaryUser = authHelper.createUser("secondary@gmail.com").let { authHelper.login(it) }
+    val tertiaryUser = authHelper.createUser("tertiary@gmail.com").let { authHelper.login(it) }
     return DefaultUsers(
       primaryUser = primaryUser, secondaryUser = secondaryUser, tertiaryUser = tertiaryUser)
   }
