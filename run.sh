0f9a2003baf4fc9659e454f7b17892ff45f94b3e
Craig Miller <craigmiller160@gmail.com>
2023-05-29 15:04:02 -0400

    Fixed run script

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/zsh
 
 # Pass in "airplane" as an argument for airplane mode
-mvn spring-boot:run -Dspring-boot.run.profiles="dev,$1"
+#mvn spring-boot:run -Dspring-boot.run.profiles="dev,$1"
+
+gradle bootRun --args="--spring.profiles.active='dev'"
