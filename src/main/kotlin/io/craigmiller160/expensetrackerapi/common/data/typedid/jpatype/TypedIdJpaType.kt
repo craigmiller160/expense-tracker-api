94b0d8f14623fb12af1a69a725dccd2d45e61d09
Craig Miller <craigmiller160@gmail.com>
2023-06-06 17:28:01 -0400

    disabled compile error

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/common/data/typedid/jpatype/TypedIdJpaType.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/common/data/typedid/jpatype/TypedIdJpaType.kt
@@ -1,29 +1,22 @@
 package io.craigmiller160.expensetrackerapi.common.data.typedid.jpatype
 
-import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
-import org.hibernate.engine.spi.SharedSessionContractImplementor
-import org.hibernate.internal.util.IndexedConsumer
-import org.hibernate.metamodel.mapping.Bindable
-import org.hibernate.type.AbstractStandardBasicType
-import org.hibernate.type.descriptor.jdbc.UUIDJdbcType
-
-class TypedIdJpaType :
-    AbstractStandardBasicType<TypedId<*>>(UUIDJdbcType.INSTANCE, TypedIdJavaType.INSTANCE) {
-  override fun getName(): String = "typed-id"
-
-  override fun forEachJdbcType(offset: Int, action: IndexedConsumer<*>?): Int {
-    super.forEachJdbcType(offset, action)
-    TODO("Not yet implemented")
-  }
-
-  override fun <X : Any?, Y : Any?> forEachDisassembledJdbcValue(
-      value: Any?,
-      offset: Int,
-      x: Any?,
-      y: Any?,
-      valuesConsumer: Bindable.JdbcValuesBiConsumer<*, *>?,
-      session: SharedSessionContractImplementor?
-  ): Int {
-    TODO("Not yet implemented")
-  }
-}
+// class TypedIdJpaType :
+//    AbstractStandardBasicType<TypedId<*>>(UUIDJdbcType.INSTANCE, TypedIdJavaType.INSTANCE) {
+//  override fun getName(): String = "typed-id"
+//
+//  override fun forEachJdbcType(offset: Int, action: IndexedConsumer<*>?): Int {
+//    super.forEachJdbcType(offset, action)
+//    TODO("Not yet implemented")
+//  }
+//
+//  override fun <X : Any?, Y : Any?> forEachDisassembledJdbcValue(
+//      value: Any?,
+//      offset: Int,
+//      x: Any?,
+//      y: Any?,
+//      valuesConsumer: Bindable.JdbcValuesBiConsumer<*, *>?,
+//      session: SharedSessionContractImplementor?
+//  ): Int {
+//    TODO("Not yet implemented")
+//  }
+// }
