a7a69cb74c46ecbf489d799b5dbfd88ee35d2431
Craig Miller <craigmiller160@gmail.com>
2023-06-06 19:10:19 -0400

    Changed default setting

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/report/ReportRequest.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/report/ReportRequest.kt
@@ -7,6 +7,6 @@ import io.craigmiller160.expensetrackerapi.web.types.PageableRequest
 data class ReportRequest(
     override val pageNumber: Int,
     override val pageSize: Int,
-    val categoryIdType: ReportCategoryIdFilterType = ReportCategoryIdFilterType.INCLUDE,
+    val categoryIdType: ReportCategoryIdFilterType = ReportCategoryIdFilterType.EXCLUDE,
     val categoryIds: List<TypedId<CategoryId>> = listOf()
 ) : PageableRequest
