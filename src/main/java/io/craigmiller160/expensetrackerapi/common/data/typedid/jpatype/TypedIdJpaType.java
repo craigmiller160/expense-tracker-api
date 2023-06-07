3c40cf624f384ff3437f7171d82576665a7e56cb
Craig Miller <craigmiller160@gmail.com>
2023-06-07 17:20:30 -0400

    Trying to make this work

--- src/main/java/io/craigmiller160/expensetrackerapi/common/data/typedid/jpatype/TypedIdJpaType.java
+++ src/main/java/io/craigmiller160/expensetrackerapi/common/data/typedid/jpatype/TypedIdJpaType.java
@@ -3,7 +3,9 @@ package io.craigmiller160.expensetrackerapi.common.data.typedid.jpatype;
 import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId;
 import org.hibernate.type.AbstractSingleColumnStandardBasicType;
 import org.hibernate.type.descriptor.jdbc.UUIDJdbcType;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TypedIdJpaType extends AbstractSingleColumnStandardBasicType<TypedId<?>> {
   public static final TypedIdJpaType INSTANCE = new TypedIdJpaType();
 
