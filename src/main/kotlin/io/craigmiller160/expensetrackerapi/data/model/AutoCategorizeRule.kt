788f2f19d76625734e9ba6f6dd2419909319809e
Craig Miller <craigmiller160@gmail.com>
2023-06-09 13:09:01 -0400

    Fixing more errors in model

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/AutoCategorizeRule.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/AutoCategorizeRule.kt
@@ -4,16 +4,22 @@ import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.AutoCategorizeRuleId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.CategoryId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.UserId
+import io.craigmiller160.expensetrackerapi.common.data.typedid.jpatype.TypedIdJavaType
 import io.craigmiller160.expensetrackerapi.data.model.core.MutableTableEntity
 import jakarta.persistence.Entity
 import jakarta.persistence.Table
 import java.math.BigDecimal
 import java.time.LocalDate
+import org.hibernate.annotations.JavaType
+import org.hibernate.annotations.JdbcType
+import org.hibernate.type.descriptor.jdbc.UUIDJdbcType
 
 @Entity
 @Table(name = "auto_categorize_rules")
 class AutoCategorizeRule(
-    var userId: TypedId<UserId>,
+    @JavaType(TypedIdJavaType::class) @JdbcType(UUIDJdbcType::class) var userId: TypedId<UserId>,
+    @JavaType(TypedIdJavaType::class)
+    @JdbcType(UUIDJdbcType::class)
     var categoryId: TypedId<CategoryId>,
     var ordinal: Int,
     var regex: String,
