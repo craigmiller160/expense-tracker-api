39806b57808f9e629edbf202dc2e10492741f119
Craig Miller <craigmiller160@gmail.com>
2023-06-10 15:01:41 -0400

    More cleanup

--- src/main/resources/sql/reports/get_total_spending_by_month_count.sql
+++ src/main/resources/sql/reports/get_total_spending_by_month_count.sql
@@ -1,13 +1,13 @@
-SELECT COUNT(DISTINCT DATE_TRUNC('month', expense_date))
-FROM transactions_view
-WHERE user_id = :userId
+SELECT COUNT(DISTINCT DATE_TRUNC('month', tv.expense_date))
+FROM transactions_view tv
+WHERE tv.user_id = :userId
 AND CASE
-    WHEN :categoryIdType = 'INCLUDE' THEN category_id IN (:categoryIds)
-    WHEN :categoryIdType = 'EXCLUDE' THEN (category_id IS NULL OR category_id NOT IN (:categoryIds))
+    WHEN :categoryIdType = 'INCLUDE' THEN tv.category_id IN (:categoryIds)
+    WHEN :categoryIdType = 'EXCLUDE' THEN (tv.category_id IS NULL OR tv.category_id NOT IN (:categoryIds))
     ELSE true = true
 END
 AND CASE
-    WHEN :unknownCategoryType = 'INCLUDE' THEN category_id IS NULL
-    WHEN :unknownCategoryType = 'EXCLUDE' THEN category_id IS NOT NULL
+    WHEN :unknownCategoryType = 'INCLUDE' THEN tv.category_id IS NULL
+    WHEN :unknownCategoryType = 'EXCLUDE' THEN tv.category_id IS NOT NULL
     ELSE true = true
 END;
