a33546d14865f7f1e858bdb1965e767fd3381e05
Craig Miller <craigmiller160@gmail.com>
2023-06-10 15:01:25 -0400

    Cleanup

--- src/main/resources/sql/reports/get_total_spending_by_month.sql
+++ src/main/resources/sql/reports/get_total_spending_by_month.sql
@@ -1,17 +1,17 @@
-SELECT DATE_TRUNC('month', expense_date) AS month, SUM(amount) as total
-FROM transactions_view
-WHERE user_id = :userId
+SELECT DATE_TRUNC('month', tv.expense_date) AS month, SUM(tv.amount) as total
+FROM transactions_view tv
+WHERE tv.user_id = :userId
 AND CASE
-    WHEN :categoryIdType = 'INCLUDE' THEN category_id IN (:categoryIds)
-    WHEN :categoryIdType = 'EXCLUDE' THEN (category_id IS NULL OR category_id NOT IN (:categoryIds))
+    WHEN :categoryIdType = 'INCLUDE' THEN tv.category_id IN (:categoryIds)
+    WHEN :categoryIdType = 'EXCLUDE' THEN (tv.category_id IS NULL OR tv.category_id NOT IN (:categoryIds))
     ELSE true = true
 END
 AND CASE
-    WHEN :unknownCategoryType = 'INCLUDE' THEN category_id IS NULL
-    WHEN :unknownCategoryType = 'EXCLUDE' THEN category_id IS NOT NULL
+    WHEN :unknownCategoryType = 'INCLUDE' THEN tv.category_id IS NULL
+    WHEN :unknownCategoryType = 'EXCLUDE' THEN tv.category_id IS NOT NULL
     ELSE true = true
 END
-GROUP BY month
-ORDER BY month DESC
+GROUP BY tv.month
+ORDER BY tv.month DESC
 OFFSET :offset
 LIMIT :limit;
