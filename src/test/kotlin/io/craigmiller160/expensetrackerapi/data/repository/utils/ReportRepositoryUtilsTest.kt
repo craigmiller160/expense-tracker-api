b2a0caa65796d2740130f9554bb1cf6ea0927bf2
Craig Miller <craigmiller160@gmail.com>
2023-06-10 17:30:55 -0400

    Implemented core of test

--- src/test/kotlin/io/craigmiller160/expensetrackerapi/data/repository/utils/ReportRepositoryUtilsTest.kt
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/data/repository/utils/ReportRepositoryUtilsTest.kt
@@ -1,7 +1,27 @@
 package io.craigmiller160.expensetrackerapi.data.repository.utils
 
+import io.craigmiller160.expensetrackerapi.web.types.report.ReportCategoryIdFilterType
+import java.util.stream.Stream
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
 class ReportRepositoryUtilsTest {
-  fun `produces correct query type`() {
-    TODO()
+  companion object {
+    @JvmStatic fun queryFilterTypeConfigs(): Stream<QueryFilterTypeConfig> = TODO()
+  }
+  @ParameterizedTest
+  @MethodSource("queryFilterTypeConfigs")
+  fun `identifies the correct query filter type`(config: QueryFilterTypeConfig) {
+    val actualResult =
+        config.categoryIdFilterType.toQueryType(config.hasUnknown, config.hasOtherIds)
+    assertEquals(config.expectedResult, actualResult)
   }
 }
+
+data class QueryFilterTypeConfig(
+    val categoryIdFilterType: ReportCategoryIdFilterType,
+    val hasUnknown: Boolean,
+    val hasOtherIds: Boolean,
+    val expectedResult: ReportQueryCategoryFilterType
+)
