ac974535203d7c5bacf4ce9d54cba00aa6ba2ff4
Craig Miller <craigmiller160@gmail.com>
2023-06-10 19:29:20 -0400

    More sql refactoring

--- src/main/resources/sql/reports/get_total_spending_by_month_count.sql
+++ src/main/resources/sql/reports/get_total_spending_by_month_count.sql
@@ -1,27 +1,4 @@
 SELECT COUNT(DISTINCT DATE_TRUNC('month', tv.expense_date))
 FROM transactions_view tv
 WHERE tv.user_id = :userId
-AND CASE
-    WHEN :categoryIdType = 'INCLUDE_NO_UNKNOWN' THEN (
-        tv.category_id IN (:categoryIds)
-    )
-    WHEN :categoryIdType = 'INCLUDE_WITH_UNKNOWN' THEN (
-        tv.category_id IN (:categoryIds) OR tv.category_id IS NULL
-    )
-    WHEN :categoryIdType = 'EXCLUDE_NO_UNKNOWN' THEN (
-        tv.category_id NOT IN (:categoryIds) AND tv.category_id IS NOT NULL
-    )
-    WHEN :categoryIdType = 'EXCLUDE_WITH_UNKNOWN' THEN (
-        tv.category_id NOT IN (:categoryIds) OR tv.category_id IS NULL
-    )
-    WHEN :categoryIdType = 'ALL_NO_UNKNOWN' THEN (
-        tv.category_id IS NOT NULL
-    )
-    WHEN :categoryIdType = 'ALL_WITH_UNKNOWN' THEN (
-        true = true
-    )
-    WHEN :categoryIdType = 'NONE_WITH_UNKNOWN' THEN (
-        tv.category_id IS NULL
-    )
-    ELSE true = true
-END;
+AND is_report_category_allowed(tv.category_id, :categoryIdType::report_category_filter_type, :categoryIds);
