4a0dd520aed0dbec2b510505e2f577d4eff7f65c
Craig Miller <craigmiller160@gmail.com>
2023-06-11 08:36:47 -0400

    Refactored all annotations to simplify setup for TypedId for JPA (#74)

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/TransactionView.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/model/TransactionView.kt
@@ -4,30 +4,23 @@ import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.CategoryId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.TransactionId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.UserId
-import io.craigmiller160.expensetrackerapi.common.data.typedid.jpatype.TypedIdJavaType
+import io.craigmiller160.expensetrackerapi.common.data.typedid.jpatype.JpaTypedId
 import io.craigmiller160.expensetrackerapi.data.model.core.ViewEntity
 import jakarta.persistence.Entity
 import jakarta.persistence.Table
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.ZonedDateTime
-import org.hibernate.annotations.JavaType
-import org.hibernate.annotations.JdbcType
-import org.hibernate.type.descriptor.jdbc.UUIDJdbcType
 
 @Entity
 @Table(name = "transactions_view")
 class TransactionView(
-    @JavaType(TypedIdJavaType::class)
-    @JdbcType(UUIDJdbcType::class)
-    override var userId: TypedId<UserId>,
+    @JpaTypedId override var userId: TypedId<UserId>,
     override var expenseDate: LocalDate,
     override var description: String,
     override var amount: BigDecimal,
     var contentHash: String,
-    @JavaType(TypedIdJavaType::class)
-    @JdbcType(UUIDJdbcType::class)
-    override var categoryId: TypedId<CategoryId>?,
+    @JpaTypedId override var categoryId: TypedId<CategoryId>?,
     var categoryName: String?,
     override var confirmed: Boolean,
     var duplicate: Boolean,
