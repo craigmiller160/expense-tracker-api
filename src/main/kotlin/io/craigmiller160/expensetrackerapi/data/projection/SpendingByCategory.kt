825de17a2b8ed7ba01fc8be8029c486bea4ee197
Craig Miller <craigmiller160@gmail.com>
2023-06-17 13:49:51 -0400

    Working on categoryId

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/data/projection/SpendingByCategory.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/data/projection/SpendingByCategory.kt
@@ -1,9 +1,12 @@
 package io.craigmiller160.expensetrackerapi.data.projection
 
+import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
+import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.CategoryId
 import java.math.BigDecimal
 import java.time.LocalDate
 
 data class SpendingByCategory(
+    val categoryId: TypedId<CategoryId>,
     val month: LocalDate,
     val categoryName: String,
     val color: String,
