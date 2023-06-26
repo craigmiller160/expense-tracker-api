a103bad05c4503ada3b2ba232eab713ad4dde968
Craig Miller <craigmiller160@gmail.com>
2023-06-26 10:46:29 -0400

    Adding some validation annotations

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/transaction/GetPossibleDuplicatesRequest.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/transaction/GetPossibleDuplicatesRequest.kt
@@ -1,7 +1,11 @@
 package io.craigmiller160.expensetrackerapi.web.types.transaction
 
 import io.craigmiller160.expensetrackerapi.web.types.PageableRequest
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
 
 // TODO needs validation
-data class GetPossibleDuplicatesRequest(override val pageNumber: Int, override val pageSize: Int) :
-    PageableRequest
+data class GetPossibleDuplicatesRequest(
+    @field:Min(0) override val pageNumber: Int,
+    @field:Max(100) override val pageSize: Int
+) : PageableRequest
