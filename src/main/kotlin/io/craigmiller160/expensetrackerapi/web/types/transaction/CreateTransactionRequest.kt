5f31e27651d11ea39f464f188cdce52658ec7a2d
Craig Miller <craigmiller160@gmail.com>
2023-06-26 10:46:00 -0400

    Cleanup

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/transaction/CreateTransactionRequest.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/transaction/CreateTransactionRequest.kt
@@ -5,7 +5,6 @@ import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.CategoryId
 import java.math.BigDecimal
 import java.time.LocalDate
 
-// TODO needs validation
 data class CreateTransactionRequest(
     val expenseDate: LocalDate,
     val description: String,
--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/transaction/DeleteTransactionsRequest.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/transaction/DeleteTransactionsRequest.kt
@@ -3,5 +3,4 @@ package io.craigmiller160.expensetrackerapi.web.types.transaction
 import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.TransactionId
 
-// TODO needs validation
 data class DeleteTransactionsRequest(val ids: Set<TypedId<TransactionId>>)
