e21d248d9ec1e6d98e9798bacd823c7846d6340a
Craig Miller <craigmiller160@gmail.com>
2023-06-26 09:25:34 -0400

    Added annotation

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/rules/AutoCategorizeRuleRequest.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/rules/AutoCategorizeRuleRequest.kt
@@ -2,12 +2,13 @@ package io.craigmiller160.expensetrackerapi.web.types.rules
 
 import io.craigmiller160.expensetrackerapi.common.data.typedid.TypedId
 import io.craigmiller160.expensetrackerapi.common.data.typedid.ids.CategoryId
+import io.craigmiller160.expensetrackerapi.data.validation.ValidRegex
 import java.math.BigDecimal
 import java.time.LocalDate
 
 data class AutoCategorizeRuleRequest(
     val categoryId: TypedId<CategoryId>,
-    val regex: String,
+    @field:ValidRegex val regex: String,
     val ordinal: Int? = null,
     val startDate: LocalDate? = null,
     val endDate: LocalDate? = null,
