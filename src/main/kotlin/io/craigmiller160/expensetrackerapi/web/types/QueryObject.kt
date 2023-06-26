3208f58a89531841ed4e991d6420f128113dc8af
Craig Miller <craigmiller160@gmail.com>
2023-06-26 08:43:06 -0400

    Refactored toQueryString

--- src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/QueryObject.kt
+++ src/main/kotlin/io/craigmiller160/expensetrackerapi/web/types/QueryObject.kt
@@ -1,33 +1,3 @@
 package io.craigmiller160.expensetrackerapi.web.types
 
-import io.craigmiller160.expensetrackerapi.common.utils.DateUtils
-import java.lang.IllegalArgumentException
-import java.net.URLEncoder
-import java.nio.charset.StandardCharsets
-import java.time.LocalDate
-
-interface QueryObject {
-  fun toQueryString(): String =
-      fieldsToQueryParams()
-          .filter { it.second != null }
-          .map { (first, second) -> first to convertValue(second!!) }
-          .joinToString("&") { "${it.first}=${it.second}" }
-
-  fun fieldsToQueryParams(): List<Pair<String, Any?>>
-}
-
-private fun convertValue(value: Any): String =
-    when (value) {
-      is Int,
-      is Long,
-      is Float,
-      is Double -> value.toString()
-      is String -> value
-      is Collection<*> ->
-          value.joinToString(",") { URLEncoder.encode(it.toString(), StandardCharsets.UTF_8) }
-      is Enum<*> -> value.name
-      is LocalDate -> DateUtils.format(value)
-      else ->
-          throw IllegalArgumentException(
-              "Invalid value to convert for query string: ${value.javaClass.name}")
-    }
+interface QueryObject
---
+++ src/test/kotlin/io/craigmiller160/expensetrackerapi/testutils/DtoUtils.kt
@@ -0,0 +1,38 @@
+package io.craigmiller160.expensetrackerapi.testutils
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonTypeRef
+import io.craigmiller160.expensetrackerapi.common.utils.DateUtils
+import io.craigmiller160.expensetrackerapi.web.types.QueryObject
+import java.lang.IllegalArgumentException
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+import java.time.LocalDate
+
+private val objectMapper = jacksonObjectMapper()
+private val mapTypeRef = jacksonTypeRef<Map<String, Any?>>()
+
+fun QueryObject.toQueryString(): String {
+  val map: Map<String, Any?> =
+      objectMapper.writeValueAsString(this).let { objectMapper.readValue(it, mapTypeRef) }
+  return map.entries
+      .filter { (_, value) -> value != null }
+      .map { (key, value) -> key to convertQueryValue(value!!) }
+      .joinToString("&") { "${it.first}=${it.second}" }
+}
+
+private fun convertQueryValue(value: Any): String =
+    when (value) {
+      is Int,
+      is Long,
+      is Float,
+      is Double -> value.toString()
+      is String -> value
+      is Collection<*> ->
+          value.joinToString(",") { URLEncoder.encode(it.toString(), StandardCharsets.UTF_8) }
+      is Enum<*> -> value.name
+      is LocalDate -> DateUtils.format(value)
+      else ->
+          throw IllegalArgumentException(
+              "Invalid value to convert for query string: ${value.javaClass.name}")
+    }
